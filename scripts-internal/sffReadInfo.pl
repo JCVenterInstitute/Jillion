c64115613cbe6630290d60f0999e91d65bac2e2b
Dan Katzel <dkatzel@users.sourceforge.net>
2010-03-01 20:30:44 +0000

    added scripts-internal folder
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@13 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

---
+++ scripts-internal/sffReadInfo.pl
@@ -0,0 +1,76 @@
+#!/usr/local/bin/perl -w
+
+use strict;
+
+use File::Basename;
+my $SCRIPT = basename($0);
+
+# #########################################################################
+#
+#    Version Strings
+#
+# #########################################################################
+my $VERSION = "0.90";
+my $BUILD = (qw/$Revision: Beta $/ )[1];
+
+# #########################################################################
+#
+#    Load Modules
+#
+# #########################################################################
+
+#
+#  Use Local Modules First
+#
+use FindBin qw($Bin);
+use lib "$Bin";
+use lib "$Bin/../perllib";
+#
+#  Standard USE statments
+#
+use Getopt::Long;
+
+#
+#  Load JavaRunner
+#
+use Java::Runner;
+
+
+# #########################################################################
+#
+#    Global Variables
+#
+# #########################################################################
+
+# #########################################################################
+#
+#    Set up the JavaRunner
+#
+# #########################################################################
+
+my $runner = Java::Runner->new();
+
+$runner->useJavaPreset("6-32b");
+
+$runner->clearClassPath();
+$runner->addNativeLibraryPath('/usr/local/n1ge/lib/lx24-x86');
+$runner->initialHeapSize(48);
+
+$runner->addClassLocation("$Bin/JavaCommon.jar");
+$runner->mainClass("org.jcvi.trace.fourFiveFour.flowgram.sff.SffReadInfo");
+#pass all arguments thru as is
+foreach my $arg (@ARGV){
+	$runner->addParameters($arg);
+}
+
+
+# #########################################################################
+#
+#    Kick off the JavaRunner
+#
+# #########################################################################
+
+
+    $runner->execute();
+
+
