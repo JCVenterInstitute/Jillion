061b632d9c11b4914d51bf11395bfc773d4e401e
Dan Katzel <dkatzel@gmail.com>
2010-03-01 20:26:06 +0000

    added support for rename contig consensus comments.  ace2fasta now will used renamed ids
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@12 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src/org/jcvi/assembly/ace/Ace2Fasta.java
+++ src/org/jcvi/assembly/ace/Ace2Fasta.java
@@ -27,12 +27,15 @@ import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.HelpFormatter;
 import org.apache.commons.cli.Options;
 import org.apache.commons.cli.ParseException;
+import org.jcvi.assembly.ace.consed.ConsedUtil;
 import org.jcvi.cli.CommandLineOptionBuilder;
 import org.jcvi.cli.CommandLineUtils;
 import org.jcvi.fasta.DefaultEncodedNucleotideFastaRecord;
@@ -61,10 +64,20 @@ public class Ace2Fasta {
                 printHelp(options);
                 System.exit(0);
             }
-            File aceIn = new File(commandLine.getOptionValue("ace"));
+            final File aceIn = new File(commandLine.getOptionValue("ace"));
     
             final OutputStream fastaOut = new FileOutputStream(commandLine.getOptionValue("out"));
             final boolean gapped = commandLine.hasOption("g");
+           
+            final Map<String, String> contigIdMap = new HashMap<String, String>();
+            DefaultAceFileTagMap tagMap = new DefaultAceFileTagMap();
+            AceFileParser.parseAceFile(aceIn, tagMap);
+            for(ConsensusAceTag consensusTag: tagMap.getConsensusTags()){
+                String originalId = consensusTag.getId();
+                if(ConsedUtil.isContigRename(consensusTag)){
+                    contigIdMap.put(originalId, ConsedUtil.getRenamedContigId(consensusTag));
+                }
+            }
             AceFileVisitor fastaVisitor = new AbstractAceFileDataStore() {
                 
                 @Override
@@ -73,8 +86,13 @@ public class Ace2Fasta {
                     if(!gapped){
                         consensus = NucleotideGlyph.convertToUngapped(consensus);
                     }
+                    String id = contigIdMap.get(contig.getId());
+                    if(id==null){
+                        id =contig.getId();
+                    }
+                    String comment = aceIn.getName()+" (whole contig)";
                     DefaultEncodedNucleotideFastaRecord fasta =
-                        new DefaultEncodedNucleotideFastaRecord(contig.getId(),
+                        new DefaultEncodedNucleotideFastaRecord(id,comment,
                     NucleotideGlyph.convertToString(consensus));
                         try {
                             fastaOut.write(fasta.getStringRecord().toString().getBytes());
@@ -84,7 +102,6 @@ public class Ace2Fasta {
                     
                 }
             };
-            
             AceFileParser.parseAceFile(aceIn, fastaVisitor);
             IOUtil.closeAndIgnoreErrors(fastaOut);
         } catch (ParseException e1) {
@@ -97,7 +114,9 @@ public class Ace2Fasta {
         HelpFormatter formatter = new HelpFormatter();
         formatter.printHelp( "ace2Fasta -ace <ace file> -out <output fasta>", 
                 
-                "create a fasta file from the contig consensus(es) of the given ace file",
+                "create a fasta file from the contig consensus(es) of the given ace file."+
+                "  The Ids used in the fasta are usually the contig ids except when "+
+                "a user added a consed 'contigName' comment (renamed a contig inside consed), then the rename is used",
                 options,
                 "Created by Danny Katzel");
     }
