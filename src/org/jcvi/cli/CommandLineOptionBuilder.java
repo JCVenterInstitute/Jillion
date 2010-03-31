edc020ed124689e3bd35d055c6455c362275f488
Dan Katzel <dkatzel@users.sourceforge.net>
2010-03-31 19:34:38 +0000

    added java doc for cli package.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@54 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src/org/jcvi/cli/CommandLineOptionBuilder.java
+++ src/org/jcvi/cli/CommandLineOptionBuilder.java
@@ -25,7 +25,15 @@ package org.jcvi.cli;
 
 import org.apache.commons.cli.Option;
 import org.jcvi.Builder;
-
+/**
+ * {@code CommandLineOptionBuilder} is an {@link Option}
+ * builder which uses a fluent interface to build Apache
+ * Commons CLI Option objects.
+ * 
+ * @author dkatzel
+ * @see Option
+ *
+ */
 public class CommandLineOptionBuilder implements Builder<Option>{
 
         private String longName;
@@ -34,29 +42,66 @@ public class CommandLineOptionBuilder implements Builder<Option>{
         private String description;
         private boolean isFlag;
         private String displayName;
+        
+        /**
+         * Begin building a new Option.
+         * @param shortName the short name for this option to be referenced
+         * by {@code -<shortName>}
+         * @param description a description of what this option does to be displayed as part
+         * of the usage statement.
+         */
         public CommandLineOptionBuilder(String shortName, String description){
             this(shortName, shortName,description);
         }
+        /**
+         * Begin building a new Option.
+         * @param shortName the short name for this option to be referenced
+         * by {@code -<shortName>}
+         * @param displayName the argument display name (used in the usage ?)
+         * @param description a description of what this option does to be displayed as part
+         * of the usage statement.
+         * 
+         */
         public CommandLineOptionBuilder(String shortName, String displayName, String description){
             this.shortName = shortName;
             this.description = description;
             this.displayName = displayName;
         }
-        
+        /**
+         * Sets if this option is a flag.
+         * @param isFlag {@code true} if this option is a flag;
+         * {@code false} for not a flag.
+         * @return this.
+         */
         public CommandLineOptionBuilder isFlag(boolean isFlag){
             this.isFlag = isFlag;
             return this;
         }
+        /**
+         * Sets if this option is a required option.
+         * @param isFlag {@code true} if this option is a required;
+         * {@code false} if not required.
+         * @return this.
+         */
         public CommandLineOptionBuilder isRequired(boolean isRequired){
             this.isRequired = isRequired;
             return this;
         }
-        
+        /**
+         * Sets long option for this name
+         * @param longname the long name for this option to be referenced
+         * by {@code --<longName>}
+         * @return this.
+         */
         public CommandLineOptionBuilder longName(String longName){
             this.longName = longName;
             return this;
         }
-
+        /**
+         * Use the data collected by this builder to construct a new
+         * {@link Option}
+         * @return a newly constructed Option with the given settings.
+         */
         @Override
         public Option build() {
             Option option = new Option(shortName, description);
