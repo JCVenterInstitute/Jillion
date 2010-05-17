0e812da2605b8a501cfbcc444d163f1017bbcf56
Dan Katzel <dkatzel@users.sourceforge.net>
2010-05-17 15:51:33 +0000

    changed MathUtil to accept Numbers and always return Longs instead of multiple overridden versions for Integer and Long etc.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@111 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- test/org/jcvi/TestMathUtilMedianOf.java
+++ test/org/jcvi/TestMathUtilMedianOf.java
@@ -34,8 +34,8 @@ public class TestMathUtilMedianOf {
     private static final String FAIL_MESSAGE = "empty list should throw illegalArgumentException";
     private static final String MUST_PASS_IN_AT_LEAST_ONE_VALUE =
                                         "must pass in at least one value";
-    private static final Integer ONE = Integer.valueOf(1);
-    private static final Integer TWO = Integer.valueOf(2);
+    private static final Long ONE = Long.valueOf(1);
+    private static final Long TWO = Long.valueOf(2);
     @Test
     public void emptyListShouldThrowIllegalArgumentException(){
         try{
@@ -50,7 +50,7 @@ public class TestMathUtilMedianOf {
     @Test
     public void emptyVarArgsShouldThrowIllegalArgumentException(){
         try{
-            MathUtil.medianOf();
+            MathUtil.<Integer>medianOf();
             fail(FAIL_MESSAGE);
         }
         catch(IllegalArgumentException e){
@@ -60,29 +60,29 @@ public class TestMathUtilMedianOf {
 
     @Test
     public void oneValue(){
-        assertEquals("one value",ONE,  MathUtil.medianOf(ONE));
+        assertEquals("one value",ONE,  MathUtil.<Long>medianOf(ONE));
     }
     @Test
     public void threeValues(){
-        assertEquals("three value",TWO,  MathUtil.medianOf(1,2,30));
+        assertEquals("three value",TWO,  MathUtil.<Integer>medianOf(1,2,30));
     }
     @Test
     public void sameValues(){
-        assertEquals("same value",TWO,  MathUtil.medianOf(1,2,2));
+        assertEquals("same value",TWO,  MathUtil.<Integer>medianOf(1,2,2));
     }
     @Test
     public void twoValues(){
-        assertEquals("same value",ONE,  MathUtil.medianOf(1,2));
+        assertEquals("same value",ONE,  MathUtil.<Integer>medianOf(1,2));
     }
 
     @Test
     public void oddValues(){
-        assertEquals("same value",Integer.valueOf(5),  MathUtil.medianOf(1, 5, 2, 8, 7));
+        assertEquals("same value",Long.valueOf(5),  MathUtil.<Integer>medianOf(1, 5, 2, 8, 7));
     }
 
     @Test
     public void evenValues(){
-        assertEquals("same value",Integer.valueOf(6),  MathUtil.medianOf(1, 5, 2, 10, 8, 7));
+        assertEquals("same value",Long.valueOf(6),  MathUtil.<Integer>medianOf(1, 5, 2, 10, 8, 7));
     }
 
 }
