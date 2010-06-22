9798f78f5b52dd62b6a170fd9cdb29d24915c7de
Dan Katzel <dkatzel@users.sourceforge.net>
2010-06-22 15:15:36 +0000

    added solexa fastQ quality codec and unit tests to read/write solexa format.  changed FastQUtil to allow for writing fastQ records without duplicating ids in quality line.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@141 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src/org/jcvi/fasta/fastq/FastQUtil.java
+++ src/org/jcvi/fasta/fastq/FastQUtil.java
@@ -22,6 +22,8 @@ package org.jcvi.fasta.fastq;
 import org.jcvi.glyph.nuc.NucleotideGlyph;
 
 /**
+ * {@code FastQUtil} is a utility class for working with 
+ * FASTQ data.
  * @author dkatzel
  *
  *
@@ -31,14 +33,27 @@ public final class FastQUtil {
     private FastQUtil(){}
     /**
      * Encode the given {@link FastQRecord} into FastQ format using the given
-     * {@link FastQQualityCodec}.
-     * @param fastQRecord the record to encode.
-     * @param qualityCodec the {@link FastQQualityCodec} to use to encode
-     * the qualities.
+     * {@link FastQQualityCodec}.  This is the same as 
+     * {@link #encode(FastQRecord, FastQQualityCodec, boolean) encode(fastQRecord,qualityCodec,false)}
      * @return a multiline string which is the fastq encoded version of the
      * given FastQRecord.
      */
     public static String encode(FastQRecord fastQRecord, FastQQualityCodec qualityCodec){
+       return encode(fastQRecord,qualityCodec,false);
+    }
+    /**
+     * Encode the given {@link FastQRecord} into FastQ format using the given
+     * {@link FastQQualityCodec}.
+     * @param fastQRecord the record to encode.
+     * @param qualityCodec the {@link FastQQualityCodec} to use to encode
+     * the qualities.
+     * @param should the read id be written (again) on the qualities line,
+     * many fastq formats no longer duplicate the id on the quality line to
+     * save space.
+     * @return a multiline string which is the fastq encoded version of the
+     * given FastQRecord.
+     */
+    public static String encode(FastQRecord fastQRecord, FastQQualityCodec qualityCodec, boolean writeIdOnQualityLine){
         String id = fastQRecord.getId();
         boolean hasComment = fastQRecord.getComment() !=null;
         
@@ -48,8 +63,11 @@ public final class FastQUtil {
         }
         builder.append("\n")
         .append(NucleotideGlyph.convertToString(fastQRecord.getNucleotides().decode())).append("\n")
-        .append("+").append(id).append("\n")
-        .append(qualityCodec.encode(fastQRecord.getQualities())).append("\n");
+        .append("+");
+        if(writeIdOnQualityLine){
+            builder.append(id);
+        }
+        builder.append("\n").append(qualityCodec.encode(fastQRecord.getQualities())).append("\n");
         return builder.toString();
     }
 }
