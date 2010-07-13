b82e2dc2ff37ed05484be74d9bb850426908118e
Dan Katzel <dkatzel@gmail.com>
2010-07-13 18:20:40 +0000

    added code to padd empty quality with 0's.  This became a problem when we convert ztrs without quals into scf which require quals even if they are 0s
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@167 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- test/org/jcvi/trace/sanger/chromatogram/TestConvertZtr2Scf.java
+++ test/org/jcvi/trace/sanger/chromatogram/TestConvertZtr2Scf.java
@@ -27,26 +27,30 @@ import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.jcvi.glyph.phredQuality.PhredQuality;
+import org.jcvi.io.fileServer.ResourceFileServer;
 import org.jcvi.trace.TraceDecoderException;
 import org.jcvi.trace.sanger.chromatogram.Chromatogram;
 import org.jcvi.trace.sanger.chromatogram.scf.SCFChromatogramImpl;
 import org.jcvi.trace.sanger.chromatogram.scf.SCFCodec;
 import org.jcvi.trace.sanger.chromatogram.scf.Version3SCFCodec;
-import org.jcvi.trace.sanger.chromatogram.scf.header.DefaultSCFHeaderCodec;
-import org.jcvi.trace.sanger.chromatogram.scf.section.DefaultSectionCodecFactory;
 import org.jcvi.trace.sanger.chromatogram.ztr.ZTRChromatogramParser;
 import org.junit.Test;
 import static org.junit.Assert.*;
 public class TestConvertZtr2Scf {
+    private static final ResourceFileServer RESOURCES = new ResourceFileServer(TestConvertZtr2Scf.class);
+    
     ZTRChromatogramParser ztrParser = new ZTRChromatogramParser();
-    SCFCodec scfCodec = new Version3SCFCodec(new DefaultSCFHeaderCodec(), new DefaultSectionCodecFactory());
+    SCFCodec scfCodec = new Version3SCFCodec();
     
     @Test
     public void ztr2scf() throws TraceDecoderException, IOException{
         
         Chromatogram decodedZTR = ztrParser.decode(
-                TestConvertZtr2Scf.class.getResourceAsStream("ztr/files/GBKAK82TF.ztr"));
+                RESOURCES.getFileAsStream("ztr/files/GBKAK82TF.ztr"));
         ByteArrayOutputStream out = new ByteArrayOutputStream();
         scfCodec.encode(new SCFChromatogramImpl(decodedZTR), out);
         
@@ -55,12 +59,35 @@ public class TestConvertZtr2Scf {
     }
     
     @Test
-    public void scfequalsZtr() throws TraceDecoderException{
+    public void scfequalsZtr() throws TraceDecoderException, IOException{
         Chromatogram decodedScf = scfCodec.decode(new DataInputStream(TestConvertZtr2Scf.class.getResourceAsStream("scf/files/GBKAK82TF.scf")));
         Chromatogram decodedZTR = ztrParser.decode(
-                TestConvertZtr2Scf.class.getResourceAsStream("ztr/files/GBKAK82TF.ztr"));
-        
-        assertEquals(decodedZTR, decodedScf);
-        
+                RESOURCES.getFileAsStream("ztr/files/GBKAK82TF.ztr"));
+        assertEquals(decodedZTR, decodedScf);        
     }
+    /**
+     * ZTR files can have no qualities (ex: trash data)
+     * but SCF requires the same # of qualities as basecalls
+     * so just set them to 0.
+     * @throws IOException 
+     * @throws TraceDecoderException 
+     */
+    @Test
+    public void ztrWithNoQualitiesShouldGetPaddedQualitiesInScf() throws TraceDecoderException, IOException{
+        Chromatogram ztr = ztrParser.decode(RESOURCES.getFileAsStream("oldTraces/515866_G07_AFIXF40TS_026.ab1.afg.trash.ztr"));
+        System.out.println(ztr.getProperties());
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+        scfCodec.encode(new SCFChromatogramImpl(ztr), out);
+        
+        Chromatogram encodedScf = scfCodec.decode(new DataInputStream(new ByteArrayInputStream(out.toByteArray())));
+        
+        int numberOfBases = (int)encodedScf.getBasecalls().getLength();
+        List<PhredQuality> expectedQualities = new ArrayList<PhredQuality>(numberOfBases);
+        for(int i=0; i< numberOfBases; i++){
+            expectedQualities.add(PhredQuality.valueOf(0));
+        }
+        assertEquals(expectedQualities,encodedScf.getQualities().decode());
+    }
+    
+    
 }
