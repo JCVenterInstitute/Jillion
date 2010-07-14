e944d8f3972c7ee16302cfb9655e9a0c41109831
Dan Katzel <dkatzel@users.sourceforge.net>
2010-07-14 20:21:04 +0000

    fixed typo in help text
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@169 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src/org/jcvi/fasta/fastq/util/SortedFasta2Fastq.java
+++ src/org/jcvi/fasta/fastq/util/SortedFasta2Fastq.java
@@ -340,7 +340,7 @@ public class SortedFasta2Fastq {
         formatter.printHelp( "sortedFasta2Fastq [OPTIONS] -s <seq file> -q <qual file> -o <fastq file>", 
                 
                 "Parse a  sorted seq and qual file (ids in same order) and write the results out a fastq file. "+
-                "This version should be orders of magnatude faster than fasta2Fastq because no indexing is required "+
+                "This version should be orders of magnitude faster than fasta2Fastq because no indexing is required "+
                 "and the seq and qual files are parsed at the same time in different threads.",
                 options,
                "Created by Danny Katzel"
