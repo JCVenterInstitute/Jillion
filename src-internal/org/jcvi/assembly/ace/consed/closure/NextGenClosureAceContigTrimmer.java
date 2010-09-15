9e285b2c797ca88acc5ea2beb54e2e0edd2a15d7
Dan Katzel <dkatzel@users.sourceforge.net>
2010-09-15 15:19:26 +0000

    added main to NextGenClosureAceContigTrimmer so it can be called separately.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@267 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src-internal/org/jcvi/assembly/ace/consed/closure/NextGenClosureAceContigTrimmer.java
+++ src-internal/org/jcvi/assembly/ace/consed/closure/NextGenClosureAceContigTrimmer.java
@@ -19,19 +19,38 @@
 
 package org.jcvi.assembly.ace.consed.closure;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.io.IOUtils;
 import org.jcvi.Range;
 import org.jcvi.assembly.ace.AceContig;
+import org.jcvi.assembly.ace.AceContigDataStore;
 import org.jcvi.assembly.ace.AceContigTrimmer;
+import org.jcvi.assembly.ace.AceFileWriter;
 import org.jcvi.assembly.ace.AcePlacedRead;
 import org.jcvi.assembly.trim.ElviraSangerContigEndTrimmer;
-import org.jcvi.assembly.trim.MinimumBidirectionalEndCoverageTrimmer;
-import org.jcvi.assembly.trim.MinimumEndCoverageTrimmer;
 import org.jcvi.assembly.trim.PlacedReadTrimmer;
+import org.jcvi.assembly.trim.TrimmerException;
+import org.jcvi.command.CommandLineOptionBuilder;
+import org.jcvi.command.CommandLineUtils;
+import org.jcvi.datastore.DataStoreException;
+import org.jcvi.datastore.MemoryMappedAceFileDataStore;
 import org.jcvi.glyph.nuc.NucleotideEncodedGlyphs;
+import org.jcvi.io.IOUtil;
+import org.jcvi.trace.sanger.phd.DefaultPhdFileDataStore;
+import org.jcvi.trace.sanger.phd.PhdDataStore;
 /**
  * @author dkatzel
  *
@@ -39,14 +58,15 @@ import org.jcvi.glyph.nuc.NucleotideEncodedGlyphs;
  */
 public class NextGenClosureAceContigTrimmer extends AceContigTrimmer{
 
+    public static final int DEFAULT_MIN_SANGER_END_CLONE_CVG=2;
+    public static final int DEFAULT_MIN_BI_DIRECTIONAL_END_COVERAGE =5;
+    public static final int DEFAULT_IGNORE_END_CVG_THRESHOLD=10;
     /**
      * @param trimmers
      */
     public NextGenClosureAceContigTrimmer(int minSangerEndCloneCoverage,int minBiDirectionalEndCoverage, int ignoreThresholdEndCoverage){
         super( Arrays.<PlacedReadTrimmer<AcePlacedRead, AceContig>>asList(
-                     //   new MinimumEndCoverageTrimmer<AcePlacedRead, AceContig>(minimumEndCoverage),
-                    //    new MinimumBidirectionalEndCoverageTrimmer<AcePlacedRead, AceContig>(minBiDirectionalEndCoverage, ignoreThresholdEndCoverage),
-                        new ElviraSangerContigEndTrimmer<AcePlacedRead, AceContig>(minSangerEndCloneCoverage,minBiDirectionalEndCoverage, ignoreThresholdEndCoverage)));
+                    new ElviraSangerContigEndTrimmer<AcePlacedRead, AceContig>(minSangerEndCloneCoverage,minBiDirectionalEndCoverage, ignoreThresholdEndCoverage)));
           
     }
 
@@ -71,5 +91,85 @@ public class NextGenClosureAceContigTrimmer extends AceContigTrimmer{
        return String.format("%s_%d_%d",originalId,_0xLeft +trimmedLeft-1, _0xLeft+trimmedRight);
     }
     
+    public static void main(String args[]) throws IOException, TrimmerException, DataStoreException{
+        Options options = new Options();
+        options.addOption(new CommandLineOptionBuilder("ace", "path to ace file")
+                .isRequired(true)
+                .build());
+        options.addOption(new CommandLineOptionBuilder("phd", "path to phd file")
+                .isRequired(true)
+                .build());
+        options.addOption(new CommandLineOptionBuilder("out", "path to new ace file")
+                .isRequired(true)
+                .build());
+        options.addOption(new CommandLineOptionBuilder("min_sanger", "min sanger end coveage default =" + DEFAULT_MIN_SANGER_END_CLONE_CVG)
+                    .build());
+        options.addOption(new CommandLineOptionBuilder("min_biDriection", "min bi directional end coveage default =" + DEFAULT_MIN_BI_DIRECTIONAL_END_COVERAGE)
+        .build());
+        
+        options.addOption(new CommandLineOptionBuilder("ignore_threshold", "min end coveage threshold to stop trying to trim default =" + DEFAULT_IGNORE_END_CVG_THRESHOLD)
+        .build());
+        
+        CommandLine commandLine;
+        PhdDataStore phdDataStore=null;
+        AceContigDataStore datastore= null;
+        try {
+            commandLine = CommandLineUtils.parseCommandLine(options, args);
+        
+        
+                int minSangerEndCloneCoverage = commandLine.hasOption("min_sanger")?
+                                            Integer.parseInt(commandLine.getOptionValue("min_sanger")):
+                                                DEFAULT_MIN_SANGER_END_CLONE_CVG;
+                
+                
+                int minBiDirectionalEndCoverage = commandLine.hasOption("min_biDriection")?
+                        Integer.parseInt(commandLine.getOptionValue("min_biDriection")):
+                            DEFAULT_MIN_BI_DIRECTIONAL_END_COVERAGE;
+        
+                int ignoreThresholdEndCoverage = commandLine.hasOption("ignore_threshold")?
+                        Integer.parseInt(commandLine.getOptionValue("ignore_threshold")):
+                            DEFAULT_IGNORE_END_CVG_THRESHOLD;
+        
+            AceContigTrimmer trimmer = new NextGenClosureAceContigTrimmer(minSangerEndCloneCoverage, minBiDirectionalEndCoverage, ignoreThresholdEndCoverage);
+            File aceFile = new File(commandLine.getOptionValue("ace"));
+            File phdFile = new File(commandLine.getOptionValue("phd"));
+            phdDataStore=new DefaultPhdFileDataStore(phdFile);
+            datastore = new MemoryMappedAceFileDataStore(aceFile);
+            File tempFile = File.createTempFile("nextGenClosureAceTrimmer", ".ace");
+            tempFile.deleteOnExit();
+            OutputStream tempOut = new FileOutputStream(tempFile);
+            int numberOfContigs=0;
+            int numberOfTotalReads=0;
+            for(AceContig contig : datastore){
+                AceContig trimmedAceContig= trimmer.trimContig(contig);
+                if(trimmedAceContig!=null){
+                    numberOfContigs++;
+                    numberOfTotalReads+=trimmedAceContig.getNumberOfReads();
+                    AceFileWriter.writeAceFile(trimmedAceContig, phdDataStore, tempOut);
+                }
+
+            }
+            IOUtil.closeAndIgnoreErrors(tempOut);
+            OutputStream masterAceOut = new FileOutputStream(new File(commandLine.getOptionValue("out")));
+            masterAceOut.write(String.format("AS %d %d%n", numberOfContigs, numberOfTotalReads).getBytes());
+            InputStream tempInput = new FileInputStream(tempFile);
+            IOUtils.copy(tempInput, masterAceOut);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            printHelp(options);
+        }finally{
+            IOUtil.closeAndIgnoreErrors(phdDataStore,datastore);
+        }
+    }
     
+    private static void printHelp(Options options){
+        HelpFormatter formatter = new HelpFormatter();
+        formatter.printHelp(
+                "NextGenClosureAceContigTrimmer -ace <ace> -phd <phd> -o <output ace> [OPTIONS]",
+                "Trim an Ace contig using closure next-gen coverage rules",
+                
+                 options,
+                 "Created by Danny Katzel");
+        System.exit(1);
+    }
 }
