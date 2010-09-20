2a987e264ebe3dc467825a837dba818851f2e47d
Dan Katzel <dkatzel@users.sourceforge.net>
2010-09-20 21:56:47 +0000

    created magicNumberInputStream
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@273 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

---
+++ test/org/jcvi/io/TestMagicNumberInputStream.java
@@ -0,0 +1,72 @@
+/*******************************************************************************
+ * Copyright 2010 J. Craig Venter Institute
+ * 
+ * 	This file is part of JCVI Java Common
+ * 
+ *     JCVI Java Common is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     JCVI Java Common is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with JCVI Java Common.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+package org.jcvi.io;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ * @author dkatzel
+ *
+ *
+ */
+public class TestMagicNumberInputStream {
+
+    byte[] data = "@MAG1234567890".getBytes();
+    
+    @Test
+    public void peak() throws IOException{
+        ByteArrayInputStream bin = new ByteArrayInputStream(data);
+        MagicNumberInputStream sut = new MagicNumberInputStream(bin);
+        assertEquals("@MAG", new String(sut.peekMagicNumber()));
+    }
+    @Test
+    public void read() throws IOException{
+        ByteArrayInputStream bin = new ByteArrayInputStream(data);
+        MagicNumberInputStream sut = new MagicNumberInputStream(bin);
+        assertTrue(Arrays.equals(data, IOUtil.readByteArray(sut, data.length)));
+    }
+    @Test
+    public void peakThenRead() throws IOException{
+        ByteArrayInputStream bin = new ByteArrayInputStream(data);
+        MagicNumberInputStream sut = new MagicNumberInputStream(bin);
+        assertEquals("@MAG", new String(sut.peekMagicNumber()));
+        assertTrue(Arrays.equals(data, IOUtil.readByteArray(sut, data.length)));
+    }
+    
+    @Test
+    public void readThenPeak() throws IOException{
+        ByteArrayInputStream bin = new ByteArrayInputStream(data);
+        MagicNumberInputStream sut = new MagicNumberInputStream(bin);
+        assertTrue(Arrays.equals(data, IOUtil.readByteArray(sut, data.length)));
+        assertEquals("@MAG", new String(sut.peekMagicNumber()));
+    }
+    @Test
+    public void differentLengthMagicNumber() throws IOException{
+        ByteArrayInputStream bin = new ByteArrayInputStream(data);
+        MagicNumberInputStream sut = new MagicNumberInputStream(bin,2);
+        assertTrue(Arrays.equals(data, IOUtil.readByteArray(sut, data.length)));
+        assertEquals("@M", new String(sut.peekMagicNumber()));
+   
+    }
+}
