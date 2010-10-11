690707a0c0f936e870023cb52a1563b1c8141c01
Dan Katzel <dkatzel@gmail.com>
2010-10-11 18:26:12 +0000

    updated to junit 4.8.2.  created new marker interfaces for specifying Fast tests, slow tests, and integration tests
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@290 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

---
+++ src/org/jcvi/testUtil/SlowTests.java
@@ -0,0 +1,33 @@
+/*******************************************************************************
+ * Copyright 2010 J. Craig Venter Institute
+ * 
+ * 	This file is part of JCVI Java Common
+ * 
+ *     JCVI Java Common is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     JCVI Java Common is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with JCVI Java Common.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+package org.jcvi.testUtil;
+
+/**
+ * {@code SlowTests} is a marker interface that can be used to
+ * categorize unit tests.  SlowTests take a while to run.  It 
+ * might be a good idea to run SlowTests either less frequently 
+ * or at the end of a suite.
+ * @author dkatzel
+ *
+ *
+ */
+public interface SlowTests {
+
+}
