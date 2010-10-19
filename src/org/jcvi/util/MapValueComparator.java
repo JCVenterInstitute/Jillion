9c43521882fdccd4414a93f18f092771a86efb28
Dan Katzel <dkatzel@users.sourceforge.net>
2010-10-19 14:11:45 +0000

    created MapValueComparator which will allow maps entries to be compared by their values instead of their keys.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@293 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

---
+++ src/org/jcvi/util/MapValueComparator.java
@@ -0,0 +1,57 @@
+/*******************************************************************************
+ * Copyright 2010 J. Craig Venter Institute
+ * 
+ * 	This file is part of JCVI Java Common
+ * 
+ *     JCVI Java Common is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     JCVI Java Common is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with JCVI Java Common.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+package org.jcvi.util;
+
+import java.util.Comparator;
+import java.util.Map;
+
+/**
+ * {@code MapValueComparator} is a {@link Comparator}
+ * that allows sorting a Map's keys based on the values mapped
+ * to the keys.
+ * @author dkatzel
+ *
+ *
+ */
+public class MapValueComparator<K,V extends Comparable> implements Comparator<K> {
+
+    private final Map<K, V> map;
+    
+    
+    
+    /**
+     * @param map
+     */
+    public MapValueComparator(Map<K, V> map) {
+        this.map = map;
+    }
+
+
+
+    /**
+    * {@inheritDoc}
+    */
+    @Override
+    public int compare(K o1, K o2) {
+        
+        return map.get(o1).compareTo(map.get(o2));
+    }
+
+}
