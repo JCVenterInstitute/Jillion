d0a5ecae7fc0e01a3bdfd7e2e2f5b3873a6346dc
Adam Resnick <resnick2206@users.sourceforge.net>
2010-10-19 22:02:31 +0000

    remove unused stub file from test area modify array job script stub to use production primer_design (/usr/local/devel/DAS/software/resequencing/prod/primer_design) scripts
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@299 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- resource/runClsrPDArrayScript.sh
+++ resource/runClsrPDArrayScript.sh
@@ -6,14 +6,14 @@ read RESULTS_RENAMED_PDF
 read PRIMER_FASTA_FILE
 
 echo "Running Primer Design now..."
-/local/ifs_devel/BCIS/src/autofinish/trunk/primer_design/PrimerDesigner/PrimerDesigner.pl -p $CONFIG_FILE
+/usr/local/devel/DAS/software/resequencing/prod/primer_design/PrimerDesigner/PrimerDesigner.pl -p $CONFIG_FILE
 
 echo "Converting GFF to PS/PDF..."
-/local/ifs_devel/BCIS/src/autofinish/trunk/primer_design/PrimerDesigner/VisualizationTools/GFF_to_PS.pl $RESULTS_GFF_FILE
+/usr/local/devel/DAS/software/resequencing/prod/primer_design/PrimerDesigner/VisualizationTools/GFF_to_PS.pl $RESULTS_GFF_FILE
 
 echo "Renaming PDF file to include template name..."
 mv $RESULTS_GFF_FILE.pdf $RESULTS_RENAMED_PDF
 
 echo "Performing Primer Critiquor as a sanity check."
-/local/ifs_devel/BCIS/src/autofinish/trunk/primer_design/PrimerDesigner/PrimerCritiquor.pl -f $PRIMER_FASTA_FILE -p $CONFIG_FILE
+/usr/local/devel/DAS/software/resequencing/prod/primer_design/PrimerDesigner/PrimerCritiquor.pl -f $PRIMER_FASTA_FILE -p $CONFIG_FILE
 
