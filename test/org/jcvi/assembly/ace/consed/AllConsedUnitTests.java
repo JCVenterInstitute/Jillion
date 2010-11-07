39ec7b9224742ffbac8f0b7d0c164aeca10c2d6b
Dan Katzel <dkatzel@gmail.com>
2010-11-07 17:03:24 +0000

    wrote unit tests for ConsedNavigationWriter
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@311 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- test/org/jcvi/assembly/ace/consed/AllConsedUnitTests.java
+++ test/org/jcvi/assembly/ace/consed/AllConsedUnitTests.java
@@ -37,7 +37,8 @@ import org.junit.runners.Suite.SuiteClasses;
         TestConsensusNavigationElement.class,
         TestConsensusNavigationElementFactoryMethod.class,
         TestReadNavigationElement.class,
-        TestConsedUtilGetNextAceVersion.class
+        TestConsedUtilGetNextAceVersion.class,
+        TestConsedNavigationWriter.class,
     }
     )
 public class AllConsedUnitTests {
