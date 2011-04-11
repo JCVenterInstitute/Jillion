d2136cc9f878f6803aea17134dc2760b92edc338
Dan Katzel <dkatzel@users.sourceforge.net>
2011-04-11 22:45:00 +0000

    more work on sffName util to add more encoding /decoding methods and tests
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@485 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

---
+++ test/org/jcvi/trace/fourFiveFour/flowgram/sff/TestSffNameUtil.java
@@ -0,0 +1,65 @@
+/*******************************************************************************
+ * Copyright 2010 J. Craig Venter Institute
+ * 
+ * 	This file is part of JCVI Java Common
+ * 
+ *     JCVI Java Common is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     JCVI Java Common is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with JCVI Java Common.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+package org.jcvi.trace.fourFiveFour.flowgram.sff;
+
+import java.util.Date;
+
+import org.joda.time.DateTime;
+import org.junit.Test;
+import static org.junit.Assert.*;
+/**
+ * @author dkatzel
+ *
+ *
+ */
+public class TestSffNameUtil {
+
+    @Test
+    public void isSffRead(){
+        assertTrue(SffNameUtil.is454Read( "C3U5GWL01CBXT2"));
+        assertFalse(SffNameUtil.is454Read("IVAAA01T48C03PB1234F"));
+        assertFalse(SffNameUtil.is454Read("IVAAA01T48HA2F"));
+    }
+    
+    @Test
+    public void parseDateFromName(){
+        DateTime expectedDate = new DateTime(2004, 9, 22, 16, 59, 10, 0);
+        assertEquals(expectedDate.toDate(), SffNameUtil.getDateOfRun("C3U5GWL01CBXT2"));
+    }
+    
+    @Test
+    public void encodeDate(){
+        DateTime expectedDate = new DateTime(2004, 9, 22, 16, 59, 10, 0);
+        assertEquals("C3U5GW", SffNameUtil.encodeDateOfRun(expectedDate.toDate()));
+    }
+    
+    @Test
+    public void parseLocation(){
+        assertEquals(new SffNameUtil.Location(838,3960), SffNameUtil.getLocationOf("C3U5GWL01CBXT2"));
+    }
+    
+    @Test
+    public void generateAccessionName(){
+        //example from 454 Data Analysis Software Manual page 533
+        String rigRunName = "R_2006_10_10_20_18_48_build04_adminrig_100x7075SEQ082806BHTF960397NewBeadDep2Region4EXP106";
+        Date expectedDate = new DateTime(2006, 10, 10, 20,18,48, 0).toDate();
+        assertEquals("EBO6PME01EE3WX", SffNameUtil.generateAccessionNumberFor(expectedDate, rigRunName, 1, new SffNameUtil.Location(1695,767)));
+    }
+}
