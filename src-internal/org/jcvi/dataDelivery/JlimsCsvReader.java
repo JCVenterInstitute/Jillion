c8261b34d8711370616c55a33f973ad17b21d880
Dan Katzel <dkatzel@gmail.com>
2011-04-29 21:23:26 +0000

    added jlims cvs reader to handle jlims reports "quirks"
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@509 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

---
+++ src-internal/org/jcvi/dataDelivery/JlimsCsvReader.java
@@ -0,0 +1,79 @@
+/*******************************************************************************
+ * Copyright 2010 J. Craig Venter Institute
+ * 
+ * 	This file is part of JCVI Java Common
+ * 
+ *     JCVI Java Common is free software: you can redistribute it and/or modify
+ *     it under the terms of the GNU General Public License as published by
+ *     the Free Software Foundation, either version 3 of the License, or
+ *     (at your option) any later version.
+ * 
+ *     JCVI Java Common is distributed in the hope that it will be useful,
+ *     but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *     GNU General Public License for more details.
+ * 
+ *     You should have received a copy of the GNU General Public License
+ *     along with JCVI Java Common.  If not, see <http://www.gnu.org/licenses/>.
+ ******************************************************************************/
+
+package org.jcvi.dataDelivery;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Iterator;
+import java.util.Map;
+
+import org.jcvi.io.CSVReader;
+import org.jcvi.io.SpreadSheetReader;
+import org.jcvi.io.TextLineParser;
+
+/**
+ * @author dkatzel
+ *
+ *
+ */
+public class JlimsCsvReader implements SpreadSheetReader{
+    private final SpreadSheetReader reader;
+    /**
+     * @param in
+     * @throws IOException 
+     */
+    public JlimsCsvReader(InputStream in) throws IOException {
+        
+        TextLineParser r = new TextLineParser(in);
+        //skip useless first line
+        String skipped =r.nextLine();
+        reader = new CSVReader(r, true);
+    }
+
+    /**
+    * {@inheritDoc}
+    */
+    @Override
+    public void close() throws IOException {
+        reader.close();
+        
+    }
+
+    /**
+    * {@inheritDoc}
+    */
+    @Override
+    public String[] getColumnNames() {
+        return reader.getColumnNames();
+    }
+
+    /**
+    * {@inheritDoc}
+    */
+    @Override
+    public Iterator<Map<String, String>> getRowIterator() {
+        return reader.getRowIterator();
+    }
+    
+    
+
+}
