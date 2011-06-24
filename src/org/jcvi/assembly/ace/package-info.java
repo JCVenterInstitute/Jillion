f8bd8a8b033200a9c8a40fa6b05370a5d603bbcb
Dan Katzel <dkatzel@gmail.com>
2011-06-24 15:55:37 +0000

    added some javadoc.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@585 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src/org/jcvi/assembly/ace/package-info.java
+++ src/org/jcvi/assembly/ace/package-info.java
@@ -19,7 +19,7 @@
 /**
  * This package contains classes for handling Contigs stored 
  * in the Ace File Format.
- * @see <a href="http://bozeman.mbt.washington.edu/consed/distributions/README.18.0.txt">ACE FILE FORMAT in Consed manual</a>
+ * @see <a href="http://www.phrap.org/consed/distributions/README.20.0.txt">ACE FILE FORMAT in Consed manual</a>
  * @author dkatzel
  */
 package org.jcvi.assembly.ace;
