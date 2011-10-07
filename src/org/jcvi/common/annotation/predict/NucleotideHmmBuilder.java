e6e7f33b250eb108f0b96575415e250b0457e394
Dan Katzel <dkatzel@users.sourceforge.net>
2011-10-07 19:15:44 +0000

    more refactoring
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@759 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src/org/jcvi/common/annotation/predict/NucleotideHmmBuilder.java
+++ src/org/jcvi/common/annotation/predict/NucleotideHmmBuilder.java
@@ -2,9 +2,7 @@ package org.jcvi.common.annotation.predict;
 
 import java.util.ArrayList;
 import java.util.EnumMap;
-import java.util.HashMap;
 import java.util.HashSet;
-import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
