194b7219e25535484be70b19e425002740ca738d
Dan Katzel <dkatzel@gmail.com>
2011-10-10 04:08:59 +0000

    Created HmmModels H3 - H27
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@764 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- test/org/jcvi/common/annotation/hmm/TestHmmModel.java
+++ test/org/jcvi/common/annotation/hmm/TestHmmModel.java
@@ -45,7 +45,7 @@ public class TestHmmModel {
 	@Test
 	public void h5_oneIntron(){
 		NucleotideSequence sequence = NucleotideSequenceFactory
-			.create("AATCATGGGGATTAGCTACGTATAGACTTATCGA");
+			.create("CATGGCGTGCAGCCCATTAGTTACTGTACCTAGT");
 		List<Gene> genes = Arrays.asList(
 					new Gene("gene 1", Range.buildRange(1,5), Range.buildRange(20,32)));
 		int[] expected = new int[]{
@@ -76,4 +76,56 @@ public class TestHmmModel {
 		assertArrayEquals(expected, actual);
 	}
 	
+	@Test
+	public void h27_oneExon(){
+		NucleotideSequence sequence = NucleotideSequenceFactory
+			.create("AATCATGGGGATTAGCTACGTATAGACTTATCGA");
+		List<Gene> genes = Arrays.asList(
+					new Gene("gene 1", Range.buildRange(4,24)));
+		int[] expected = new int[]{
+				1,1,1,1,2,3,4,5,6,7,5,6,7,5,6,7,5,6,7,5,6,7,8,9,10,1,1,1,1,1,1,1,1,1
+		};
+		
+		int[] actual = HmmModels.H27.labelSequence(sequence, genes);
+		
+		assertArrayEquals(expected, actual);
+	}
+	
+	@Test
+	public void h27_oneIntron(){
+		NucleotideSequence sequence = NucleotideSequenceFactory
+			.create("CATGGCGTGCAGCCCATTAGTTACTGTACCTAGT");
+		List<Gene> genes = Arrays.asList(
+					new Gene("gene 1", Range.buildRange(1,5), Range.buildRange(20,32)));
+		int[] expected = new int[]{
+			  //C A T G G C G  T  G  C  A  G  C  C  C  A  T  T  A  G  T T A C T G T A C C T A G  T
+			  //N E E E E E I  I  I  I  I  I  I  I  I  I  I  I  I  I  E E E E E E E E E E E E E  N
+				1,2,3,4,5,6,18,19,20,20,20,20,20,20,20,20,20,20,21,22,7,5,6,7,5,6,7,5,6,7,8,9,10,1
+			};
+		
+		int[] actual = HmmModels.H27.labelSequence(sequence, genes);
+		
+		assertArrayEquals(expected, actual);
+	}
+	@Test
+	public void h27_manyIntrons(){
+		NucleotideSequence sequence = NucleotideSequenceFactory
+			.create("GCATGGTCTTAGGTGTATCGACAGTGTAGTAATC");
+		List<Gene> genes = Arrays.asList(
+					new Gene("gene 1", 
+							Range.buildRange(2,4),
+							Range.buildRange(12,13),
+							Range.buildRange(24),
+							Range.buildRange(29,31)							
+					));
+		int[] expected = new int[]{
+			  //G C A T G G  T  C  T  T  A  G  G T G  T  A  T  C  G  A  C  A  G  T G  T  A  G  T A A  T C
+			  //N N E E E I  I  I  I  I  I  I  E E I  I  I  I  I  I  I  I  I  I  E I  I  I  I  E E E  N N
+				1,1,2,3,4,23,24,25,25,25,26,27,5,6,18,19,20,20,20,20,20,20,21,22,7,23,24,26,27,8,9,12,1,1,
+		};
+		
+		int[] actual = HmmModels.H27.labelSequence(sequence, genes);
+		
+		assertArrayEquals(expected, actual);
+	}
 }
