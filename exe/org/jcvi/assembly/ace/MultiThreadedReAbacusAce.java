412e9370917e39c72eff24ca12d0273f7cc478b2
Dan Katzel <dkatzel@gmail.com>
2011-11-07 21:02:50 +0000

    renamed class serialReAbacusAce -> multithreadedReAbacusAce.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@845 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- exe/org/jcvi/assembly/ace/MultiThreadedReAbacusAce.java
+++ exe/org/jcvi/assembly/ace/MultiThreadedReAbacusAce.java
@@ -82,7 +82,7 @@ import org.jcvi.common.core.util.iter.CloseableIterator;
  *
  *
  */
-public class SerialReAbacusAce {
+public class MultiThreadedReAbacusAce {
     private static final int DEFAULT_FLANK_LENGTH = 20;
 
     /**
@@ -201,7 +201,7 @@ public class SerialReAbacusAce {
      */
     private static void printHelp(Options options) {
         HelpFormatter formatter = new HelpFormatter();
-        formatter.printHelp( "serialReAbacusAce -a <ace file> -nav <input nav file> -o <ace out file> -num_threads <num>", 
+        formatter.printHelp( "multiThreadedReAbacusAce -a <ace file> -nav <input nav file> -o <ace out file> -num_threads <num>", 
                 
                 "Parse an ace file and input nav file with abacus problem regions and write out a new ace file " +
                 "with those regions realigned and consensus recalled to try to correct the problem regions.",
