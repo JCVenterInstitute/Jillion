7bf60885ae1d98c4f8a4988f841be9603643a993
Dan Katzel <dkatzel@users.sourceforge.net>
2011-12-21 21:16:58 +0000

    added a few more tests for DefaultZipDataStore
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@921 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- test/org/jcvi/common/io/zip/TestDefaultZipDataStore.java
+++ test/org/jcvi/common/io/zip/TestDefaultZipDataStore.java
@@ -2,13 +2,40 @@ package org.jcvi.common.io.zip;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
 
+import org.jcvi.common.core.datastore.DataStoreException;
+import org.junit.Test;
+import static org.easymock.EasyMock.*;
+import static org.junit.Assert.*;
 public class TestDefaultZipDataStore extends AbstractTestZipDataStore{
 
 	@Override
 	protected ZipDataStore createZipDataStore(File file) throws IOException {
 		return new DefaultZipDataStore(new ZipFile(file));
 	}
+	
+	@Test(expected = NullPointerException.class)
+	public void nullConstructorShouldThrowNPE(){
+		new DefaultZipDataStore(null);
+	}
 
+	@Test
+	public void errorWhileGettingEntryShouldThrowException() throws IOException{
+		ZipFile mockFile = createMock(ZipFile.class);
+		ZipEntry mockEntry = createMock(ZipEntry.class);
+		expect(mockFile.getEntry("id")).andReturn(mockEntry);
+		IOException expectedException = new IOException("expected");
+		expect(mockFile.getInputStream(mockEntry)).andThrow(expectedException);
+		replay(mockFile, mockEntry);
+		
+		DefaultZipDataStore datastore = new DefaultZipDataStore(mockFile);
+		try {
+			datastore.get("id");
+			fail("should throw exception if zipEntry throws exception");
+		} catch (DataStoreException e) {
+			assertEquals(expectedException, e.getCause());
+		}
+	}
 }
