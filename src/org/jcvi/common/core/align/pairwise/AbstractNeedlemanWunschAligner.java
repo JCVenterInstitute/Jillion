7db54ae355f5462b37ac6adb3134f6ef716070c7
Dan Katzel <dkatzel@gmail.com>
2012-01-24 15:49:36 +0000

    Added AminoAcidNeedlemanWunsch support more refactoring and tests.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@946 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src/org/jcvi/common/core/align/pairwise/AbstractNeedlemanWunschAligner.java
+++ src/org/jcvi/common/core/align/pairwise/AbstractNeedlemanWunschAligner.java
@@ -12,6 +12,49 @@ abstract class AbstractNeedlemanWunschAligner <R extends Residue, S extends Sequ
 			float extendGapPenalty) {
 		super(query, subject, matrix, openGapPenalty, extendGapPenalty);
 	}
+	/**
+	 * The initial gap scores of NeedlemanWunsch are the values
+	 * returned by the affine gap penalty.
+	 * </p>
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected float[] getInitialGapScores(int length, float openGapPenalty,
+			float extendGapPenalty) {
+		float[] array = new float[length];
+		
+		array[0]=0;
+		array[1]=openGapPenalty;
+		float currentScore=openGapPenalty;
+		for(int i=2; i< length; i++){
+			currentScore +=extendGapPenalty;
+			array[i] = currentScore;
+		}
+		return array;
+	}
+
+	/**
+	 * Since Needleman-Wunsch is a global alignment
+	 * we need to keep gapping until we get to the beginning
+	 * of the sequence so keep tracing horizontally.
+	 * @return TracebackDirection#HORIZONTAL
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected TracebackDirection getInitialRowTracebackValue() {
+		return TracebackDirection.HORIZONTAL;
+	}
+	/**
+	 * Since Needleman-Wunsch is a global alignment
+	 * we need to keep gapping until we get to the beginning
+	 * of the sequence so keep tracing horizontally.
+	 * @return TracebackDirection#VERTICAL
+	 * {@inheritDoc}
+	 */
+	@Override
+	protected TracebackDirection getInitialColTracebackValue() {
+		return TracebackDirection.VERTICAL;
+	}
 
 	@Override
 	protected BestWalkBack computeBestWalkBack(float alignmentScore,
@@ -28,19 +71,15 @@ abstract class AbstractNeedlemanWunschAligner <R extends Residue, S extends Sequ
 			}
 			return new BestWalkBack(bestScore, dir);
 	}
+	/**
+	 * Always update the CurrentStartPointer to the given values.
+	 * <p/>
+	 * {@inheritDoc}
+	 */
 	@Override
 	protected CurrentStartPoint updateCurrentStartPoint(float currentBestScore,
 			CurrentStartPoint currentStartPoint, int i, int j) {
 		return new CurrentStartPoint(i, j, currentBestScore);
-	}
-
-	@Override
-	protected float getInitialRowScore(float[] bestPathSoFar, int rowNumber,
-			float openGapPenalty, float extendGapPenalty) {
-		if(rowNumber==1){
-			return 0;
-		}
-		return openGapPenalty + (rowNumber-1)*extendGapPenalty;
 	}
 	
 	
