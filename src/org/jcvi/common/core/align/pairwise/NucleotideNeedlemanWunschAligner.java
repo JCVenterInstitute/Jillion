96077d9b0f641d29cfc6b7969b16646aaecaa517
Dan Katzel <dkatzel@gmail.com>
2012-02-02 16:42:21 +0000

    added javadoc with links to URLs of the famous paper publications.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@960 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src/org/jcvi/common/core/align/pairwise/NucleotideNeedlemanWunschAligner.java
+++ src/org/jcvi/common/core/align/pairwise/NucleotideNeedlemanWunschAligner.java
@@ -4,16 +4,46 @@ import org.jcvi.common.core.align.NucleotideSequenceAlignment;
 import org.jcvi.common.core.symbol.Sequence;
 import org.jcvi.common.core.symbol.residue.nuc.Nucleotide;
 import org.jcvi.common.core.symbol.residue.nuc.NucleotideSequence;
-
+/**
+ * {@code NucleotideNeedlemanWunschAligner} can perform 
+ * a pair-wise alignment of two {@link NucleotideSequence}s
+ * using the global alignment algorithm developed
+ * by Needleman and Wunsch using improvements developed
+ * by Gotoh.
+ * @author dkatzel
+ * 
+ * @see <a href="http://dx.doi.org/10.1016/0022-2836(70)90057-4">
+ Needleman, Saul B.; and Wunsch, Christian D.
+ "A general method applicable to the search for similarities in the amino acid sequence of two proteins".
+ Journal of Molecular Biology 48:443-53.</a>
+ *   
+ * @see <a href="http://dx.doi.org/10.1016/0022-2836(82)90398-9">
+ Gotoh Osamu. An improved algorithm for matching biological sequences. 
+ Journal of Molecular Biology 162:705-708</a>
+ */
 public final class NucleotideNeedlemanWunschAligner extends AbstractNeedlemanWunschAligner<Nucleotide, NucleotideSequence, NucleotideSequenceAlignment, NucleotidePairwiseSequenceAlignment>{
-
-	public static NucleotidePairwiseSequenceAlignment align(Sequence<Nucleotide> query,
-			Sequence<Nucleotide> subject, ScoringMatrix<Nucleotide> matrix,
+	/**
+	 * Align the given two {@link NucleotideSequence}s
+	 * using the given {@link ScoringMatrix} by the Needleman-Wunsch
+	 * global alignment algorithm.
+	 * @param query the query {@link NucleotideSequence} to align;
+	 * can not be null.
+	 * @param subject the subject {@link NucleotideSequence} to align;
+	 * can not be null.
+	 * @param matrix the {@link ScoringMatrix} to use; can not be null.
+	 * @param openGapPenalty the penalty value for opening a gap.
+	 * @param extendGapPenalty the penalty for extending an already open gap.
+	 * @return a new {@link NucleotidePairwiseSequenceAlignment} instance;
+	 * representing the global alignment, will never be null.
+	 * @throws NullPointerException if query, subject or matrix are null.
+	 */
+	public static NucleotidePairwiseSequenceAlignment align(NucleotideSequence query,
+			NucleotideSequence subject, ScoringMatrix<Nucleotide> matrix,
 			float openGapPenalty, float extendGapPenalty){
 		NucleotideNeedlemanWunschAligner aligner = new NucleotideNeedlemanWunschAligner(query, subject, matrix, openGapPenalty, extendGapPenalty);
 		return aligner.getPairwiseSequenceAlignment();
 				
-		}
+	}
 	
 	private NucleotideNeedlemanWunschAligner(Sequence<Nucleotide> query,
 			Sequence<Nucleotide> subject, ScoringMatrix<Nucleotide> matrix,
