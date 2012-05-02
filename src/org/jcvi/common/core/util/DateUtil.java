4027462167e6709f563f48bb6e24d948206e12e8
Dan Katzel <dkatzel@gmail.com>
2012-05-02 20:49:15 +0000

    added javadoc
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@1145 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src/org/jcvi/common/core/util/DateUtil.java
+++ src/org/jcvi/common/core/util/DateUtil.java
@@ -10,26 +10,43 @@ import java.util.Date;
  */
 public final class DateUtil {
 
-	private static long specifiedTime;
-	private static boolean useSystemTime=true;
+	private static long SPECIFIED_TIME;
+	private static boolean USE_SYSTEM_TIME=true;
 	private DateUtil(){}
-	
+	/**
+	 * Get the current Date from either the system
+	 * or whatever Date has been specified by
+	 * {@link #setCurrentDate(Date)}.
+	 * @return a new {@link Date}; never null.
+	 * @see #setCurrentDate(Date)
+	 */
 	public static synchronized Date getCurrentDate(){
-		if(useSystemTime){
+		if(USE_SYSTEM_TIME){
 			return new Date();
 		}
-		return new Date(specifiedTime);
+		return new Date(SPECIFIED_TIME);
 	}
-	
+	/**
+	 * Set the Date to return when {@link #getCurrentDate()}
+	 * is called. Copies of this Date will be returned by
+	 * {@link #getCurrentDate()} until either a new Date is set
+	 * or until {@link #restoreDate()} is called.
+	 * @param date the Date to use; can not be null.
+	 * @see #restoreDate()
+	 * @see #getCurrentDate()
+	 */
 	public static synchronized void setCurrentDate(Date date){
 		if(date ==null){
 			throw new NullPointerException("specified date can not be null");
 		}
-		useSystemTime=false;
-		specifiedTime = date.getTime();
+		USE_SYSTEM_TIME=false;
+		SPECIFIED_TIME = date.getTime();
 	}
-	
+	/**
+	 * Change {@link #getCurrentDate()} to return the 
+	 * current system time.
+	 */
 	public static synchronized void restoreDate(){
-		useSystemTime=true;
+		USE_SYSTEM_TIME=true;
 	}
 }
