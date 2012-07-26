5af9a635fb879dc8deca0d14e0c20c687012d9bb
Dan Katzel <dkatzel@users.sourceforge.net>
2012-07-26 19:34:48 +0000

    changed NucloetideSequenceFastaRecord to longer be a child of AbstractFastaRecord. improved performance of Nucleotides.parse(String) bumped up version number to java common 3.6.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@1238 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src/org/jcvi/common/core/seq/fastx/fasta/nt/NucleotideSequenceFastaRecord.java
+++ src/org/jcvi/common/core/seq/fastx/fasta/nt/NucleotideSequenceFastaRecord.java
@@ -23,27 +23,39 @@
  */
 package org.jcvi.common.core.seq.fastx.fasta.nt;
 
-import org.jcvi.common.core.seq.fastx.fasta.AbstractFastaRecord;
+import java.util.regex.Pattern;
+
 import org.jcvi.common.core.seq.fastx.fasta.FastaRecord;
 import org.jcvi.common.core.seq.fastx.fasta.FastaUtil;
 import org.jcvi.common.core.symbol.residue.nt.Nucleotide;
 import org.jcvi.common.core.symbol.residue.nt.NucleotideSequence;
 import org.jcvi.common.core.symbol.residue.nt.NucleotideSequenceBuilder;
+import org.jcvi.common.core.util.ObjectsUtil;
 /**
  * {@code NucleotideSequenceFastaRecord} is an implementation
  * of {@link FastaRecord} whose sequences are {@link NucleotideSequence}s.
  * @author dkatzel
  *
  */
-public final class NucleotideSequenceFastaRecord extends AbstractFastaRecord<Nucleotide,NucleotideSequence> implements FastaRecord<Nucleotide,NucleotideSequence>{
+public final class NucleotideSequenceFastaRecord implements FastaRecord<Nucleotide,NucleotideSequence>{
 
+	private static final int NUMBER_OF_BASES_PER_LINE = 60;
+	private static final Pattern LINE_SPLITTER_PATTERN = Pattern.compile(String.format("(.{%s})", NUMBER_OF_BASES_PER_LINE));
+	private static final String LINE_SPLITTER_REPLACEMENT = "$1"+FastaUtil.LINE_SEPARATOR;
+	
 	private final NucleotideSequence sequence;
-   
-    public NucleotideSequenceFastaRecord(String identifier, NucleotideSequence sequence){
-        this(identifier, null, sequence);
+	private final String id;
+	private final String comments;
+	
+    public NucleotideSequenceFastaRecord(String id, NucleotideSequence sequence){
+        this(id, null, sequence);
     }
-    public NucleotideSequenceFastaRecord(String identifier, String comments, NucleotideSequence sequence){
-    	 super(identifier, comments);
+    public NucleotideSequenceFastaRecord(String id, String comments, NucleotideSequence sequence){
+    	if(id == null){
+            throw new IllegalArgumentException("identifier can not be null");
+        }
+        this.id = id;
+        this.comments = comments;
          if(sequence ==null){
          	throw new NullPointerException("sequence can not be null");
          }
@@ -52,60 +64,119 @@ public final class NucleotideSequenceFastaRecord extends AbstractFastaRecord<Nuc
    
     
     /**
-     * @param identifier
+     * @param id
      * @param sequence
      */
-    public NucleotideSequenceFastaRecord(String identifier, CharSequence sequence) {
-        this(identifier, null, sequence);
+    public NucleotideSequenceFastaRecord(String id, String sequence) {
+        this(id, null, sequence);
     }
 
     /**
-     * @param identifier
+     * @param id
      * @param comments
      * @param sequence
      */
-    public NucleotideSequenceFastaRecord(String identifier, String comments,
-            CharSequence sequence) {
-    	super(identifier, comments);
-        String nonWhiteSpaceSequence = sequence.toString().replaceAll("\\s+", "");
-        this.sequence = new NucleotideSequenceBuilder(nonWhiteSpaceSequence).build();
+    public NucleotideSequenceFastaRecord(String id, String comments,
+            String sequence) {
+    	this(id, comments, new NucleotideSequenceBuilder(sequence).build());
+    }
+    /**
+     * @return A <code>String</code>.
+     */
+    public String getId()
+    {
+        return this.id;
     }
 
+    /**
+     * @return A <code>String</code>.
+     */
+    public String getComment()
+    {
+        return this.comments;
+    }
     @Override
     public NucleotideSequence getSequence() 
     {
         return this.sequence;
     }
-    
-    
-    @Override
-    protected CharSequence getRecordBody()
+    public String toFormattedString()
     {
-        String result= this.sequence.toString().replaceAll("(.{60})", "$1"+FastaUtil.LINE_SEPARATOR);
+    	int bufferSize = computeFormattedBufferSize();
+        final StringBuilder record = new StringBuilder(bufferSize);
+        record.append(FastaUtil.HEADER_PREFIX).append(
+                this.getId());
+        if (this.getComment() != null) {
+        	record.append(' ').append(this.getComment());
+        }
+        record.append(FastaUtil.LINE_SEPARATOR);
+        record.append(this.getRecordBody());
+        record.append(FastaUtil.LINE_SEPARATOR);
+        
+        return record.toString();
+    }
+    
+    private int computeFormattedBufferSize() {
+    	//2 extra bytes for '>' and '\n'
+		int size = 2 + id.length();
+		if(comments!=null){
+			//extra byte for the space
+			size +=1 + comments.length();
+		}
+		int seqLength=(int)sequence.getLength();
+		int numberOfLines = seqLength/NUMBER_OF_BASES_PER_LINE +1;
+		return size + seqLength+numberOfLines;
+	}
+	
+    
+    /**
+     * 
+    * Gets the entire formatted fasta record as a String,
+    * same as {@link #toFormattedString()}.
+    * @see #toFormattedString()
+     */
+    @Override
+    public String toString()
+    {
+        return this.toFormattedString();
+    }
+    
+    private String getRecordBody()
+    {
+        String result= LINE_SPLITTER_PATTERN.matcher(this.sequence.toString()).replaceAll(LINE_SPLITTER_REPLACEMENT);
         //some fasta parsers such as blast's formatdb
         //break if there is an extra blank line between records
         //this can happen if the sequence ends at the exact length of 1 line
-        //(60 characters)
         long length = sequence.getLength();
-        if(length >0 && length%60==0){
+        if(length >0 && length%NUMBER_OF_BASES_PER_LINE==0){
             return result.substring(0, result.length()-1);
         }
         return result;
     }
     
-    
-
     @Override
-    public boolean equals(Object obj) {
-        if(!(obj instanceof NucleotideSequenceFastaRecord)){
+    public int hashCode()
+    {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + this.id.hashCode();
+        result = prime * result + this.getSequence().hashCode();
+        return result;
+    }
+    
+    @Override
+    public boolean equals(Object obj)
+    {
+        if (this == obj){
+            return true;
+        }
+        if (!(obj instanceof NucleotideSequenceFastaRecord)){
             return false;
         }
-        return super.equals(obj);
-    }
-
-    @Override
-    public int hashCode() {
-        return super.hashCode();
-    }
+        NucleotideSequenceFastaRecord other = (NucleotideSequenceFastaRecord)obj;
+		return 
+        ObjectsUtil.nullSafeEquals(getSequence(), other.getSequence()) 
+        && ObjectsUtil.nullSafeEquals(getId(), other.getId());
+    }   
    
 }
