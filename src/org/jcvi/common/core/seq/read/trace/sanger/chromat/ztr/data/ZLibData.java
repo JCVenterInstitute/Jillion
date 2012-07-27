9f81e53e367f3add930d2f093c779e5d3fff3ac0
Dan Katzel <dkatzel@users.sourceforge.net>
2012-07-27 14:54:06 +0000

    added calls to inflater#end() and defalater#end() in finally blocks to avoid memory leaks.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@1243 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src/org/jcvi/common/core/seq/read/trace/sanger/chromat/ztr/data/ZLibData.java
+++ src/org/jcvi/common/core/seq/read/trace/sanger/chromat/ztr/data/ZLibData.java
@@ -61,7 +61,7 @@ public enum ZLibData implements Data {
      */
     @Override
     public byte[] parseData(byte[] data) throws TraceDecoderException {
-
+    	Inflater decompresser=null;
         try{
             ByteBuffer size = ByteBuffer.allocate(4);
             size.put(data,1,4);    
@@ -69,15 +69,21 @@ public enum ZLibData implements Data {
             ByteBuffer compressedData = ByteBuffer.allocate(data.length-5);
             compressedData.put(data,5,data.length-5);
             //decompress the zipped data
-            Inflater decompresser = new Inflater();
+            decompresser = new Inflater();
             decompresser.setInput(compressedData.array());
             ByteBuffer byteBuffer = ByteBuffer.allocate(uncompressedLength);
             decompresser.inflate(byteBuffer.array());
             
-            decompresser.end();
+            
             return byteBuffer.array();
         }catch(DataFormatException dfEx){
             throw new TraceDecoderException("could not parse ZLibData",dfEx);
+        }finally{
+        	//call end in finally block to avoid
+        	//memory leak incase of exception thrown.
+        	if(decompresser !=null){
+        		decompresser.end();
+        	}
         }
     }
 
@@ -85,19 +91,25 @@ public enum ZLibData implements Data {
 	public byte[] encodeData(byte[] data) throws TraceEncoderException {
 		
 		Deflater zlibCompresser= new Deflater();
-		zlibCompresser.setInput(data);
-		zlibCompresser.finish();
-		byte[] compressedData = new byte[data.length*2];
-		int numberOfCompressedBytes =zlibCompresser.deflate(compressedData);
-		//5 = header length + uncompressed size as int
-		ByteBuffer encodedBuffer = ByteBuffer.allocate(numberOfCompressedBytes+5);
-		encodedBuffer.put(DataHeader.ZLIB_ENCODED);
-		//for some reason length is different endian...
-		byte size[] =IOUtil.switchEndian(IOUtil.convertUnsignedIntToByteArray(data.length));
-		encodedBuffer.put(size);
-		encodedBuffer.put(compressedData,0, numberOfCompressedBytes);
-		encodedBuffer.flip();
-		return Arrays.copyOfRange(encodedBuffer.array(), 0, encodedBuffer.limit());
+		try{
+			zlibCompresser.setInput(data);
+			zlibCompresser.finish();
+			byte[] compressedData = new byte[data.length*2];
+			int numberOfCompressedBytes =zlibCompresser.deflate(compressedData);
+			//5 = header length + uncompressed size as int
+			ByteBuffer encodedBuffer = ByteBuffer.allocate(numberOfCompressedBytes+5);
+			encodedBuffer.put(DataHeader.ZLIB_ENCODED);
+			//for some reason length is different endian...
+			byte size[] =IOUtil.switchEndian(IOUtil.convertUnsignedIntToByteArray(data.length));
+			encodedBuffer.put(size);
+			encodedBuffer.put(compressedData,0, numberOfCompressedBytes);
+			encodedBuffer.flip();
+			return Arrays.copyOfRange(encodedBuffer.array(), 0, encodedBuffer.limit());
+		}finally{
+			//always call end() to avoid
+			//memory leaks
+			zlibCompresser.end();
+		}
 	}
 	/**
 	 * This returns the same result as {@link #encodeData(byte[])}
