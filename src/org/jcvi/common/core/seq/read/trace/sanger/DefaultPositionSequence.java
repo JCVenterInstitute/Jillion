64b93afbc9b4e8308d733befa8639413e0dd2bfe
Dan Katzel <dkatzel@gmail.com>
2012-08-07 19:09:17 +0000

    changed PositionSequenceBuilder to use new DefaultPositionSequence implementation which should be faster
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@1274 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src/org/jcvi/common/core/seq/read/trace/sanger/DefaultPositionSequence.java
+++ src/org/jcvi/common/core/seq/read/trace/sanger/DefaultPositionSequence.java
@@ -1,35 +1,79 @@
 package org.jcvi.common.core.seq.read.trace.sanger;
 
 import java.util.Iterator;
-import java.util.List;
+import java.util.NoSuchElementException;
 
 import org.jcvi.common.core.Range;
+import org.jcvi.common.core.io.IOUtil;
 
 public class DefaultPositionSequence implements PositionSequence{
 
+	private final short[] data;
+	
+	DefaultPositionSequence(short[] data) {
+		this.data = data;
+	}
 
 	@Override
 	public Position get(int index) {
-		// TODO Auto-generated method stub
-		return null;
+		return Position.valueOf(IOUtil.toUnsignedShort(data[index]));
 	}
 
 	@Override
 	public long getLength() {
-		// TODO Auto-generated method stub
-		return 0;
+		return data.length;
 	}
 
 	@Override
 	public Iterator<Position> iterator(Range range) {
-		// TODO Auto-generated method stub
-		return null;
+		return new IteratorImpl(range);
 	}
 
 	@Override
 	public Iterator<Position> iterator() {
-		// TODO Auto-generated method stub
-		return null;
+		return new IteratorImpl();
 	}
 
+	private class IteratorImpl implements Iterator<Position>{
+		private final int end;
+		private int offset;
+		
+		IteratorImpl(){
+			end = data.length;
+			offset=0;
+		}
+		IteratorImpl(Range range){
+			
+			offset = (int)range.getBegin();
+			end = (int)range.getEnd();
+			if(offset<0){
+				throw new IllegalArgumentException(String.format("range %s can not have negative values",range));
+			}
+			if(end>data.length){
+				throw new IllegalArgumentException(
+						String.format("range %s can not extend past sequence length %d",range,data.length));
+			}
+		}
+		@Override
+		public boolean hasNext() {
+			return offset<end;
+		}
+		@Override
+		public Position next() {
+			if(!hasNext()){
+				throw new NoSuchElementException(String.format("offset = %d"));
+			}
+			Position next = get(offset);
+			offset++;
+			return next;
+		}
+		@Override
+		public void remove() {
+			throw new UnsupportedOperationException();
+			
+		}
+		
+		
+		
+	}
 }
