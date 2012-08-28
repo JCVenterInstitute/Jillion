c602e112665500028cecc91da80f28f9af8719e9
Dan Katzel <dkatzel@users.sourceforge.net>
2012-08-28 19:27:04 +0000

    added tests for aminoAcidSequenceFastaRecord datastore iterator
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@1337 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- test/org/jcvi/common/core/seq/fastx/fasta/aa/AbstractTestAminoAcidSequenceFastaDataStore.java
+++ test/org/jcvi/common/core/seq/fastx/fasta/aa/AbstractTestAminoAcidSequenceFastaDataStore.java
@@ -1,7 +1,6 @@
 package org.jcvi.common.core.seq.fastx.fasta.aa;
 
 import java.io.File;
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.Iterator;
 
@@ -104,5 +103,38 @@ public abstract class AbstractTestAminoAcidSequenceFastaDataStore {
 	@Test
 	public void getLastRecord() throws DataStoreException{
 		assertEquals(lastRecord, sut.get("EPI159954"));
+	}
+	
+	@Test
+	public void iterator() throws DataStoreException{
+		//we aren't going to check each record.
+		//we will assume that if the order
+		//is correct and our first, middle
+		//and last records are correct and in the 
+		//correct order then the other records
+		//in between are correct as well.
+		StreamingIterator<AminoAcidSequenceFastaRecord> iter= sut.iterator();
+		
+		try{
+			assertTrue(iter.hasNext());
+			assertEquals(firstRecord, iter.next());
+			assertTrue(iter.hasNext());
+			//skip to the next record we know about
+			for(int i=0; i<4; i++){
+				assertTrue(iter.hasNext());
+				assertNotNull(iter.next());
+			}
+			assertEquals(middleRecord, iter.next());
+			assertTrue(iter.hasNext());
+			//skip to the last record
+			for(int i=0; i<6; i++){
+				assertTrue(iter.hasNext());
+				assertNotNull(iter.next());
+			}
+			assertEquals(lastRecord, iter.next());
+			assertFalse(iter.hasNext());
+		}finally{
+			IOUtil.closeAndIgnoreErrors(iter);
+		}
 	}
 }
