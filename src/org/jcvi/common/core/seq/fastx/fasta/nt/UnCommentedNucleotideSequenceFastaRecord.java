6419b1e06272312564fcc23ba6c0e4203136dbde
Dan Katzel <dkatzel@gmail.com>
2012-09-04 19:04:29 +0000

    removed Fasta#toFormattedString() and changed Fasta#toString() to no longer print formatted string, now just prints debug info line.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@1347 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src/org/jcvi/common/core/seq/fastx/fasta/nt/UnCommentedNucleotideSequenceFastaRecord.java
+++ src/org/jcvi/common/core/seq/fastx/fasta/nt/UnCommentedNucleotideSequenceFastaRecord.java
@@ -1,8 +1,6 @@
 package org.jcvi.common.core.seq.fastx.fasta.nt;
 
-import java.util.regex.Pattern;
 
-import org.jcvi.common.core.seq.fastx.fasta.FastaUtil;
 import org.jcvi.common.core.symbol.residue.nt.NucleotideSequence;
 import org.jcvi.common.core.util.ObjectsUtil;
 /**
@@ -15,10 +13,6 @@ import org.jcvi.common.core.util.ObjectsUtil;
  */
 class UnCommentedNucleotideSequenceFastaRecord implements NucleotideSequenceFastaRecord{
 
-	private static final int NUMBER_OF_BASES_PER_LINE = 60;
-	private static final Pattern LINE_SPLITTER_PATTERN = Pattern.compile(String.format("(.{%s})", NUMBER_OF_BASES_PER_LINE));
-	private static final String LINE_SPLITTER_REPLACEMENT = "$1"+FastaUtil.LINE_SEPARATOR;
-	
 	private final NucleotideSequence sequence;
 	private final String id;
 
@@ -55,59 +49,17 @@ class UnCommentedNucleotideSequenceFastaRecord implements NucleotideSequenceFast
     {
         return this.sequence;
     }
-    private String toFormattedString()
-    {
-    	int bufferSize = computeFormattedBufferSize();
-        final StringBuilder record = new StringBuilder(bufferSize);
-        record.append(FastaUtil.HEADER_PREFIX).append(
-                this.getId());
-        if (this.getComment() != null) {
-        	record.append(' ').append(this.getComment());
-        }
-        record.append(FastaUtil.LINE_SEPARATOR);
-        record.append(this.getRecordBody());
-        record.append(FastaUtil.LINE_SEPARATOR);
-        
-        return record.toString();
-    }
-    
-    private int computeFormattedBufferSize() {
-    	//2 extra bytes for '>' and '\n'
-		int size = 2 + id.length();
-		if(getComment()!=null){
-			//extra byte for the space
-			size +=1 + getComment().length();
-		}
-		int seqLength=(int)sequence.getLength();
-		int numberOfLines = seqLength/NUMBER_OF_BASES_PER_LINE +1;
-		return size + seqLength+numberOfLines;
-	}
+
 	
     
-    /**
-     * 
-    * Gets the entire formatted fasta record as a String,
-    * same as {@link #toFormattedString()}.
-    * @see #toFormattedString()
-     */
     @Override
-    public String toString()
-    {
-        return this.toFormattedString();
-    }
-    
-    private String getRecordBody()
-    {
-        String result= LINE_SPLITTER_PATTERN.matcher(this.sequence.toString()).replaceAll(LINE_SPLITTER_REPLACEMENT);
-        //some fasta parsers such as blast's formatdb
-        //break if there is an extra blank line between records
-        //this can happen if the sequence ends at the exact length of 1 line
-        long length = sequence.getLength();
-        if(length >0 && length%NUMBER_OF_BASES_PER_LINE==0){
-            return result.substring(0, result.length()-1);
-        }
-        return result;
-    }
+   	public String toString() {
+   		return "NucleotideSequenceFastaRecord [id=" + getId()
+   				+ ", comment=" + getComment() + ", sequence ="
+   				+ getSequence() + "]";
+   	}
+       
+  
     
     @Override
     public int hashCode()
--- src/org/jcvi/common/core/seq/fastx/fasta/qual/UncommentedQualitySequenceFastaRecord.java
+++ src/org/jcvi/common/core/seq/fastx/fasta/qual/UncommentedQualitySequenceFastaRecord.java
@@ -1,9 +1,5 @@
 package org.jcvi.common.core.seq.fastx.fasta.qual;
 
-import java.util.Iterator;
-
-import org.jcvi.common.core.seq.fastx.fasta.FastaUtil;
-import org.jcvi.common.core.symbol.qual.PhredQuality;
 import org.jcvi.common.core.symbol.qual.QualitySequence;
 
 class UncommentedQualitySequenceFastaRecord implements QualitySequenceFastaRecord{
@@ -41,66 +37,15 @@ class UncommentedQualitySequenceFastaRecord implements QualitySequenceFastaRecor
 	}
 
 
-	private String toFormattedString()
-    {
-        final StringBuilder record = new StringBuilder();
-        
-        record.append(this.getRecordHeader());
-        appendCarriageReturnAndLineFeed(record)
-        .append(this.getRecordBody());
-        appendCarriageReturnAndLineFeed(record);
-        
-        return record.toString();
-    }
     
-   
-    protected CharSequence getRecordHeader()
-    {
-        final StringBuilder result = new StringBuilder();
-        result.append(FastaUtil.HEADER_PREFIX).append(
-                this.getId());
-        if (this.getComment() != null) {
-            result.append(' ').append(this.getComment());
-        }
-        return result;
-    }
-    
-    protected StringBuilder appendCarriageReturnAndLineFeed(StringBuilder s){
-        return s.append(FastaUtil.LINE_SEPARATOR);
-        
-    }
-    
-    /**
-     * 
-    * Gets the entire formatted fasta record as a String,
-    * same as {@link #toFormattedString()}.
-    * @see #toFormattedString()
-     */
     @Override
-    public String toString()
-    {
-        return this.toFormattedString();
-    }
+	public String toString() {
+		return "QualitySequenceFastaRecord [id=" + getId()
+				+ ", comment=" + getComment() + ", sequence ="
+				+ getSequence() + "]";
+	}
     
-    protected CharSequence getRecordBody() {
-        int length = (int)qualities.getLength();
-		StringBuilder result = new StringBuilder(3*length);
-		Iterator<PhredQuality> iter = qualities.iterator();
-        int i=1;
-        while(iter.hasNext()){
-        	result.append(String.format("%02d", iter.next().getQualityScore()));
-        	if(iter.hasNext()){
-        		if(i%17 == 0){
-                    this.appendCarriageReturnAndLineFeed(result);
-                }
-                else{
-                    result.append(' ');
-                }   
-        	}
-        	i++;
-        }
-        return result.toString();     
-    }
+    
 
     @Override
     public QualitySequence getSequence() {
