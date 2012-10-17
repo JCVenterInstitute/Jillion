d7cb693e17bd9ccc110be7fcc49fe03d228c0150
Dan Katzel <dkatzel@users.sourceforge.net>
2012-10-17 21:21:40 +0000

    simplified get and contains calls to delegate to iterator.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@1455 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src/org/jcvi/common/core/seq/fastx/fasta/nt/LargeNucleotideSequenceFastaFileDataStore.java
+++ src/org/jcvi/common/core/seq/fastx/fasta/nt/LargeNucleotideSequenceFastaFileDataStore.java
@@ -23,14 +23,11 @@
  */
 package org.jcvi.common.core.seq.fastx.fasta.nt;
 
-import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.util.Scanner;
 import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 import org.jcvi.common.core.datastore.AcceptingDataStoreFilter;
 import org.jcvi.common.core.datastore.CachedDataStore;
@@ -38,7 +35,7 @@ import org.jcvi.common.core.datastore.DataStoreException;
 import org.jcvi.common.core.datastore.DataStoreFilter;
 import org.jcvi.common.core.datastore.DataStoreStreamingIterator;
 import org.jcvi.common.core.io.IOUtil;
-import org.jcvi.common.core.seq.fastx.fasta.FastaFileParser;
+import org.jcvi.common.core.seq.fastx.FastXFilter;
 import org.jcvi.common.core.seq.fastx.fasta.FastaUtil;
 import org.jcvi.common.core.seq.fastx.fasta.LargeFastaIdIterator;
 import org.jcvi.common.core.util.iter.StreamingIterator;
@@ -53,9 +50,7 @@ import org.jcvi.common.core.util.iter.StreamingIterator;
  * @author dkatzel
  */
 final class LargeNucleotideSequenceFastaFileDataStore implements NucleotideSequenceFastaDataStore{
-	
-	
-	private static final Pattern NEXT_ID_PATTERN = Pattern.compile("^>(\\S+)");
+
     private final File fastaFile;
     private final DataStoreFilter filter;
     private Long size;
@@ -115,40 +110,24 @@ final class LargeNucleotideSequenceFastaFileDataStore implements NucleotideSeque
     @Override
     public boolean contains(String id) throws DataStoreException {
         checkNotYetClosed();
-        InputStream in=null;
-        try {
-            in= getRecordFor(id);
-            return in !=null;
-        } catch (FileNotFoundException e) {
-           throw new DataStoreException("could not get record for "+id,e);
-        }finally{
-           IOUtil.closeAndIgnoreErrors(in);
-        }
+        return get(id)!=null;
     }
 
     @Override
     public NucleotideSequenceFastaRecord get(String id)
             throws DataStoreException {
-        checkNotYetClosed();
-        InputStream in=null;
-        NucleotideSequenceFastaDataStore datastore=null;
-        try {
-            in = getRecordFor(id);
-        
-            if(in ==null){
-                return null;
-            }
-      
-            NucleotideFastaDataStoreBuilderVisitor builder= DefaultNucleotideSequenceFastaFileDataStore.createBuilder(filter);
-            FastaFileParser.parse(in, builder);
-            datastore = builder.build();
-            return datastore.get(id);
-           
-        } catch (FileNotFoundException e) {
-            throw new DataStoreException("could not get record for "+id, e);
-        }
-        finally{
-            IOUtil.closeAndIgnoreErrors(in,datastore);
+        StreamingIterator<NucleotideSequenceFastaRecord> iter = iterator();
+        try{
+	        while(iter.hasNext()){
+	        	NucleotideSequenceFastaRecord next = iter.next();
+	        	if(next.getId().equals(id)){
+	        		return next;
+	        	}
+	        }
+	        //we get here if we didn't find it
+	        return null;
+        }finally{
+        	IOUtil.closeAndIgnoreErrors(iter);
         }
     }
 
@@ -164,19 +143,7 @@ final class LargeNucleotideSequenceFastaFileDataStore implements NucleotideSeque
         checkNotYetClosed();
         if(size ==null){
             try {
-                Scanner scanner = new Scanner(fastaFile, IOUtil.UTF_8_NAME);
-                long counter =0;
-                while(scanner.hasNextLine()){
-                    String line = scanner.nextLine();
-                    Matcher matcher = NEXT_ID_PATTERN.matcher(line);
-                    if(matcher.find()){
-                    	String id = matcher.group(1);
-                    	if(filter.accept(id)){
-                    		counter++;
-                    	}
-                    }
-                }
-                size= counter;            
+                size= countFilteredIds();            
             } catch (FileNotFoundException e) {
                 throw new IllegalStateException("could not get record count");
             }
@@ -184,65 +151,37 @@ final class LargeNucleotideSequenceFastaFileDataStore implements NucleotideSeque
         return size;
 
     }
+	private long countFilteredIds() throws FileNotFoundException {
+		Scanner scanner = new Scanner(fastaFile, IOUtil.UTF_8_NAME);
+		long counter =0;
+		while(scanner.hasNextLine()){
+		    String line = scanner.nextLine();
+		    
+		    Matcher matcher = FastaUtil.ID_LINE_PATTERN.matcher(line);
+		    if(matcher.find()){
+		    	String id = matcher.group(1);
+		    	final boolean accept;
+		    	if(filter instanceof FastXFilter){
+		    		accept = ((FastXFilter)filter).accept(id, matcher.group(2));
+		    	}else{
+		    		accept = filter.accept(id);
+		    	}
+		    	if(accept){
+		    		counter++;
+		    	}
+		    }
+		}
+		return counter;
+	}
 
     @Override
     public StreamingIterator<NucleotideSequenceFastaRecord> iterator() {
         checkNotYetClosed();
-        return DataStoreStreamingIterator.create(this,LargeNucleotideSequenceFastaIterator.createNewIteratorFor(fastaFile));
+        return DataStoreStreamingIterator.create(this,
+        		LargeNucleotideSequenceFastaIterator.createNewIteratorFor(fastaFile));
        
     }
-    /**
-     * Get the part of the large fasta file we care about.
-     * @param id the id of the fasta record we want.
-     * @return an {@link InputStream} containing <strong>only</strong>
-     * the fasta record we care about; or null if no such record exists.
-     * @throws FileNotFoundException if the fasta file is no longer
-     * available to read.
-     */
-    private InputStream getRecordFor(String id) throws FileNotFoundException{
-        Scanner scanner = null;
-        try{
-            scanner = new Scanner(fastaFile, IOUtil.UTF_8_NAME);
-            String expectedHeader = String.format(">%s", id);
-            String line = scanner.nextLine();
-            boolean done=false;
-            //we have to do this while loop to make sure we find
-            //the actual read instead of a different read which is happens
-            //to include our id as a prefix (for example a TIGR "B" read)
-            while(!done){
-                if(line.startsWith(expectedHeader)){
-                    String currentId= FastaUtil.parseIdFromDefLine(line);
-                    if(id.equals(currentId)){
-                        done=true;
-                        //done
-                        continue;
-                    }
-                }
-                if(!scanner.hasNextLine()){
-                    done=true;
-                    continue;
-                }
-                line = scanner.nextLine(); 
-            }
-            
-            if(!scanner.hasNextLine()){
-                return null;
-            }
-            StringBuilder record = new StringBuilder(line).append('\n');
-            line =scanner.nextLine();
-            while(!line.startsWith(">") && scanner.hasNextLine()){
-                record.append(line).append('\n');
-                line = scanner.nextLine();
-            }
-            //add final line if needed
-            if(!scanner.hasNextLine()){
-                record.append(line).append('\n');
-            }
-            return new ByteArrayInputStream(record.toString().getBytes(IOUtil.UTF_8));
-        }finally{
-            IOUtil.closeAndIgnoreErrors(scanner);
-        }
-    }
+   
 
 
    
