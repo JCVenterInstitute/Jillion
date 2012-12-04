4b6140e50d9ca7d88c0faad2f34471d4e624068f
Dan Katzel <dkatzel@users.sourceforge.net>
2012-12-04 16:03:13 +0000

    more work on primer Tm calculations now match DNA mate after confusing several santaLucia and Allawi papers.  Added all relavent papers I could find on the web since they aren't all in pubmed.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@1542 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src/org/jcvi/common/primer/SaltCorrectionStrategy.java
+++ src/org/jcvi/common/primer/SaltCorrectionStrategy.java
@@ -5,13 +5,30 @@ public enum SaltCorrectionStrategy {
 	 * The used salt correction
 	 * used most often in programs
 	 * like primer3.
-	 * The actual correction equation
+	 * The correction equation is 
+	 * <pre>16.6 * log10(concentration in nM) + initialTm</pre>
+	 * The implementation will convert the input mM into nM.
+	 * This equation
 	 * was derived by
 	 * 
 	 * C C Schildkraut Biopolymers 3(2):195-208 (1965), PMID 5889540.
 	 */
 	SCHILDKRAUT_LIFSON(16.6D),
-	SANTALUCIA(12.5D)
+	/**
+	 * The used salt correction
+	 * listed in the santaLucia paper
+	 * The correction equation is 
+	 * <pre>12.5 * log10(concentration in nM) + initialTm</pre>
+	 * The implementation will convert the input mM into nM.
+	 * This equation
+	 * was derived by
+	 * SantaLucia96
+	 * @see <a href="http://www.ncbi.nlm.nih.gov/pubmed/8639506">
+	 SantaLucia J Jr, Allawi HT, Seneviratne PA.
+	 Improved nearest-neighbor parameters for predicting DNA duplex stability.
+	Biochemistry. 1996 Mar 19;35(11):3555-62; PMID 8639506 
+	 */
+	SANTALUCIA_1996(12.5D)
 	;
 	
 	private final double adjustmentConstant;
