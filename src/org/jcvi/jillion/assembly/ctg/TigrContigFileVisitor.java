ad7cfedecf1ffc41f7299861d7a6561af7eeae91
Dan Katzel <dkatzel@users.sourceforge.net>
2013-01-25 02:44:05 +0000

    started work on new implementations of TigrContigParser and visitors
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@1671 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

---
+++ src/org/jcvi/jillion/assembly/ctg/TigrContigFileVisitor.java
@@ -0,0 +1,50 @@
+package org.jcvi.jillion.assembly.ctg;
+
+public interface TigrContigFileVisitor {
+
+	public interface TigrContigVisitorCallback{
+		/**
+		 * {@code TigrContigVisitorMemento} is a marker
+		 * interface that {@link TigrContigFileParser}
+		 * instances can use to "rewind" back
+		 * to the position in its contig file
+		 * in order to revisit portions of the contig file. 
+		 * {@link TigrContigVisitorMemento} should only be used
+		 * by the {@link TigrContigFileParser} instance that
+		 * generated it.
+		 * @author dkatzel
+		 *
+		 */
+		public interface TigrContigVisitorMemento{
+			
+		}
+		/**
+		 * Is this callback capable of
+		 * creating {@link TigrContigVisitorMemento}s
+		 * via {@link #createMemento()}.
+		 * @return {@code true} if this callback
+		 * can create mementos; {@code false} otherwise.
+		 */
+		boolean canCreateMemento();
+		/**
+		 * Create a {@link TigrContigVisitorMemento}
+		 * 
+		 * @return a {@link TigrContigVisitorMemento}; never null.
+		 * @see #canCreateMemento()
+		 * @throws UnsupportedOperationException if {@link #canCreateMemento()}
+		 * returns {@code false}.
+		 */
+		TigrContigVisitorMemento createMemento();
+		/**
+		 * Tell the {@link TigrContigFileParser} to stop parsing
+		 * the fastq file.  {@link TigrContigFileVisitor#visitEnd()}
+		 * will still be called.
+		 */
+		void stopParsing();
+	}
+	
+	
+	TigrContigVisitor visitContig(String contigId);
+	
+	void visitEnd();
+}
---
+++ src/org/jcvi/jillion/assembly/ctg/TigrContigReadVisitor.java
@@ -0,0 +1,14 @@
+package org.jcvi.jillion.assembly.ctg;
+
+import org.jcvi.jillion.core.Direction;
+import org.jcvi.jillion.core.Range;
+import org.jcvi.jillion.core.residue.nt.NucleotideSequence;
+
+public interface TigrContigReadVisitor {
+
+	
+	void visitValidRange(Range validRange);
+	
+	void visitBasecalls(NucleotideSequence gappedBasecalls);
+	void visitEnd();
+}
