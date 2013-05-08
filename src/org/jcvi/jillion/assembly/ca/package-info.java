92c687c8dcf73dd2359f9c45f4add7739b2fd8eb
Dan Katzel <dkatzel@gmail.com>
2013-05-08 20:47:43 +0000

    added package info to Celera Assembler package
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@1866 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

---
+++ src/org/jcvi/jillion/assembly/ca/package-info.java
@@ -0,0 +1,28 @@
+/*******************************************************************************
+ * Copyright (c) 2013 J. Craig Venter Institute.
+ * 	This file is part of Jillion
+ * 
+ * 	 Jillion is free software: you can redistribute it and/or modify
+ * 	it under the terms of the GNU General Public License as published by
+ * 	the Free Software Foundation, either version 3 of the License, or
+ * 	(at your option) any later version.
+ * 	
+ * 	 Jillion is distributed in the hope that it will be useful,
+ * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * 	GNU General Public License for more details.
+ * 	
+ * 	You should have received a copy of the GNU General Public License
+ * 	along with  Jillion.  If not, see http://www.gnu.org/licenses
+ * 
+ * Contributors:
+ *     Danny Katzel - initial API and implementation
+ ******************************************************************************/
+/**
+ * This package contains objects 
+ * for working with Celera Assembler input 
+ * and output.
+ * 
+ * <a href= "http://wgs-assembler.sourceforge.net/"> Celera Assembler on Sourceforge</a>
+ */
+package org.jcvi.jillion.assembly.ca;
