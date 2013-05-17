11a23f18bb5af6beade984d21d83b099d9ca35c6
Dan Katzel <dkatzel@users.sourceforge.net>
2013-05-17 16:05:04 +0000

    Created new TasmWriter and TasmFileWriterBuilder classes that support both normal and annotation tasm contigs.  Made old tasm writer with its static methods a package private utility class (that the new writer delegates to)
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@1881 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

---
+++ src/org/jcvi/jillion/assembly/tigr/tasm/TasmFileWriterBuilder.java
@@ -0,0 +1,129 @@
+package org.jcvi.jillion.assembly.tigr.tasm;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.jcvi.jillion.core.io.IOUtil;
+import org.jcvi.jillion.core.util.Builder;
+/**
+ * {@code TasmFileWriterBuilder} builds a {@link TasmWriter}
+ * using which has been configured by the the parameters given
+ * to its various methods.
+ * @author dkatzel
+ *
+ */
+public final class TasmFileWriterBuilder implements Builder<TasmWriter>{
+
+	private final File outputFile;
+	private OutputStream out;
+	
+	private boolean annotationContigs=false;
+	/**
+	 * Create a new Builder which will build a 
+	 * {@link TasmWriter}
+	 *  that will write its data to the given
+	 *  output file.
+	 * @param outputTasmFile the output File to write the data
+	 * to; can not be null.  If this file already exists, then the old data will
+	 * be overwritten, if this file does not exist or if
+	 * any parent directories for this file do not exist,
+	 * then they will be created when the TasmWriter is built.
+	 * @throws NullPointerException if outputTasmFile is null.
+	 */
+	public TasmFileWriterBuilder(File outputTasmFile){
+		if(outputTasmFile ==null){
+			throw new NullPointerException("output file can not be null");
+		}
+		this.outputFile = outputTasmFile;
+		this.out = null;
+	}
+	/**
+	 * Configure the TasmWriter to write 
+	 * "TIGR annotation contigs" which are used
+	 * by the annotation teams.  This type of contig
+	 * differs from a normal contig, in that the
+	 * annotation contig only has the consensus and related
+	 * metadata, the underlying read information is not
+	 * included in the tasm output.  
+	 * <strong>This method should only be used
+	 * internally by JCVI</strong> since only
+	 * internal JCVI utilities require annotation tasm files.
+	 * 
+	 * @return
+	 */
+	public TasmFileWriterBuilder writeAnnotationContigs(){
+		this.annotationContigs=true;
+		return this;
+	}
+	/**
+	 * Create a new Builder which will build a 
+	 * {@link TasmWriter}
+	 *  that will write its data to the given
+	 *  {@link OutputStream}.
+	 * @param out the{@link OutputStream} to write the data
+	 * to; can not be null.
+	 * @throws NullPointerException if outputTasmFile is null.
+	 */
+	public TasmFileWriterBuilder(OutputStream out){
+		if(out ==null){
+			throw new NullPointerException("outputStream can not be null");
+		}
+		this.outputFile = null;
+		this.out = out;
+	}
+	
+	@Override
+	public TasmWriter build() {
+		if(outputFile ==null){
+			return new TasmFileWriter2(out, annotationContigs);
+		}else{
+			try {
+				return new TasmFileWriter2(outputFile, annotationContigs);
+			} catch (IOException e) {
+				throw new IllegalStateException("error creating tasm writer",e);
+			}
+		}
+	}
+
+	private static final class TasmFileWriter2 implements TasmWriter{
+
+		private final OutputStream out;
+		private final boolean writeAnnotationContigs;
+		private int numberOfContigsWritten=0;
+		
+		public TasmFileWriter2(File outputFile,boolean writeAnnotationContigs) throws IOException{
+			IOUtil.mkdirs(outputFile);
+			out = new BufferedOutputStream(new FileOutputStream(outputFile));
+			this.writeAnnotationContigs = writeAnnotationContigs;
+		}
+		
+		public TasmFileWriter2(OutputStream out,boolean writeAnnotationContigs){
+			
+			this.out = out;
+			this.writeAnnotationContigs = writeAnnotationContigs;
+		}
+		
+		@Override
+		public void close() throws IOException {
+			out.close();			
+		}
+
+		@Override
+		public void write(TasmContig contig) throws IOException {
+			if(numberOfContigsWritten>0){
+				TasmFileWriterUtil.writeContigSeparator(out);
+			}
+			if(writeAnnotationContigs){
+				TasmFileWriterUtil.writeAnnotationRecord(contig, out);
+			}else{
+				TasmFileWriterUtil.write(contig, out);
+			}
+			numberOfContigsWritten++;
+			
+		}
+		
+	}
+}
