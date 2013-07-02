28e8d00a2ccb881c82a9322347bb553cad50fee5
Dan Katzel <dkatzel@users.sourceforge.net>
2013-07-02 20:21:23 +0000

    initial check in of new Codon and Translation classes.  tests are only for standard translation tables although others are supported.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@1972 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

---
+++ test/org/jcvi/jillion/core/residue/nt/TestTriplet.java
@@ -0,0 +1,89 @@
+package org.jcvi.jillion.core.residue.nt;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertSame;
+
+import org.jcvi.jillion.core.testUtil.TestUtil;
+import org.junit.Test;
+public class TestTriplet {
+
+	Triplet sut = Triplet.create('A','T','G');
+	
+	@Test
+	public void constructor(){
+		assertEquals(Nucleotide.Adenine,sut.getFirst());
+		assertEquals(Nucleotide.Thymine,sut.getSecond());
+		assertEquals(Nucleotide.Guanine,sut.getThrid());
+	}
+	@Test
+	public void notEqualToNull(){
+		assertFalse(sut.equals(null));
+	}
+	
+	@Test
+	public void notDifferentObject(){
+		assertFalse(sut.equals("not a triplet"));
+	}
+	
+	@Test
+	public void nucleotideFactoryMethod(){
+		Triplet t = Triplet.create(Nucleotide.Adenine, Nucleotide.Thymine, Nucleotide.Guanine);
+		assertEquals(sut, t);
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void nullFirstShouldThrowNPE(){
+		Triplet.create(null, Nucleotide.Thymine, Nucleotide.Guanine);
+	}
+	@Test(expected = NullPointerException.class)
+	public void nullSecondShouldThrowNPE(){
+		Triplet.create(Nucleotide.Adenine, null, Nucleotide.Guanine);
+	}
+	@Test(expected = NullPointerException.class)
+	public void nullThirdShouldThrowNPE(){
+		Triplet.create(Nucleotide.Adenine, Nucleotide.Thymine, null);
+	}
+	@Test
+	public void testToString(){
+		assertEquals("ATG",sut.toString());
+	}
+	@Test
+	public void equalsSameRef(){
+		TestUtil.assertEqualAndHashcodeSame(sut, sut);
+	}
+	@Test
+	public void equalsSameValues(){
+		Triplet.clearCache();
+		Triplet sameValues = Triplet.create('A','T','G');
+		TestUtil.assertEqualAndHashcodeSame(sut, sameValues);
+	}
+	
+	@Test
+	public void sameValuesReturnSameReference(){
+		//because we've cleared the cache in a different test
+		//and can't guarentee (nor should we want to)
+		//that the cache was cleared, it is easier
+		//to get more references and make sure they match
+		//then compare the sut
+		
+		Triplet ref1 = Triplet.create('A','T','G');
+		Triplet ref2 = Triplet.create('A','T','G');
+		assertSame(ref1, ref2);
+	}
+	@Test
+	public void differentFirstValueShouldNotBeEqual(){
+		Triplet diffValues = Triplet.create('G', 'T', 'G');
+		TestUtil.assertNotEqualAndHashcodeDifferent(sut, diffValues);
+	}
+	@Test
+	public void differentSecondValueShouldNotBeEqual(){
+		Triplet diffValues = Triplet.create('A', 'C', 'G');
+		TestUtil.assertNotEqualAndHashcodeDifferent(sut, diffValues);
+	}
+	@Test
+	public void differentThridValueShouldNotBeEqual(){
+		Triplet diffValues = Triplet.create('A', 'T', 'R');
+		TestUtil.assertNotEqualAndHashcodeDifferent(sut, diffValues);
+	}
+}
