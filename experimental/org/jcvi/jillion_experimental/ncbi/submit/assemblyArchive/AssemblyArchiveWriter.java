b8738ee7e4f2bbd42f3caeeb017422b4f9ec3b63
Dan Katzel <dkatzel@gmail.com>
2013-07-19 18:00:49 +0000

    renamed method of TraceNameLookup.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@2019 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- experimental/org/jcvi/jillion_experimental/ncbi/submit/assemblyArchive/AssemblyArchiveWriter.java
+++ experimental/org/jcvi/jillion_experimental/ncbi/submit/assemblyArchive/AssemblyArchiveWriter.java
@@ -9,7 +9,20 @@ import org.jcvi.jillion.assembly.Contig;
 public interface AssemblyArchiveWriter extends Closeable{
 
 	public interface TraceNameLookup{
-		String getTraceNameByContigId (String contigReadId);
+		/**
+		 * Get the trace name to use in the Assembly archive.
+		 * This trace name must match the corresponding 
+		 * name in the Trace Archive.  Very often this trace name
+		 * is different than the read id that is commonly used in the 
+		 * contig assembly.
+		 * @param readId the read id; can not be null and must
+		 * have a corresponding trace name.
+		 * @return a String representation of the trace name.
+		 * @throws IllegalArgumentException if the lookup 
+		 * can not find a valid trace name for the given read id.
+		 * @throws NullPointerException if readId is null.
+		 */
+		String getTraceNameByContigReadId (String readId);
 	}
 	
 	void write(
