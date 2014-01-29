a61c9174535bce7f08d2c51336f5874f71eb30f6
Dan Katzel <dkatzel@gmail.com>
2014-01-29 20:38:43 +0000

    added javadoc
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@2156 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src/org/jcvi/jillion/assembly/AssemblyTransformer.java
+++ src/org/jcvi/jillion/assembly/AssemblyTransformer.java
@@ -7,27 +7,92 @@ import org.jcvi.jillion.core.pos.PositionSequence;
 import org.jcvi.jillion.core.qual.QualitySequence;
 import org.jcvi.jillion.core.residue.nt.NucleotideSequence;
 import org.jcvi.jillion.core.residue.nt.ReferenceMappedNucleotideSequence;
-
+/**
+ * {@code AssemblyTransformer} is an interface
+ * to transform one assembly format into another.
+ * <p>
+ * {@code AssemblyTransformer}
+ * is a higher level abstraction of an assembly
+ * above a  lower-level Visitor because it hides
+ * file format specific information
+ * @author dkatzel
+ *
+ */
 public interface AssemblyTransformer {
-
-	void addReferenceOrConsensus(String id, NucleotideSequence gappedReference);
-
+	/**
+	 * The reference or consensus sequence used in the assembly.
+	 * @param id the reference or contig consensus id; can not be null.
+	 * @param gappedReference the gapped {@link NucleotideSequence}
+	 */
+	void referenceOrConsensus(String id, NucleotideSequence gappedReference);
+	/**
+	 * The end of the assembly has been reached.
+	 */
 	void endAssembly();
-
+	/**
+	 * This read did not align to any reference or 
+	 * contig consensus.
+	 * @param id the read id.
+	 * @param nucleotideSequence the {@link NucleotideSequence};
+	 * can not be null.
+	 * @param qualitySequence the {@link QualitySequence} for this
+	 * read; may be {@code null} if not known.
+	 * @param positions the {@link PositionSequence} for this
+	 * read; may be {@code null} if not known or if there are no
+	 * positions (for example if this read is not sanger).
+	 * @param uri the {@link URI} for the location of the file
+	 * that contains this read; may be {@code null} if not known.
+	 */
 	void notAligned(String id, NucleotideSequence nucleotideSequence,
 			QualitySequence qualitySequence, PositionSequence positions, URI uri);
-
+	/**
+	 * This read aligned to a particular reference at the given location.
+	 * @param id the read id.
+	 * @param nucleotideSequence the {@link NucleotideSequence};
+	 * can not be null.
+	 * @param qualitySequence the {@link QualitySequence} for this
+	 * read; may be {@code null} if not known.
+	 * @param positions the {@link PositionSequence} for this
+	 * read; may be {@code null} if not known or if there are no
+	 * positions (for example if this read is not sanger).
+	 * @param sourceFileUri the {@link URI} for the location of the file
+	 * that contains this read; may be {@code null} if not known.
+	 * @param referenceId the reference id this read aligned to.  This id
+	 * must be mentioned previously by a call to {@link #referenceOrConsensus(String, NucleotideSequence).
+	 * @param gappedStartOffset the gapped start offset (0-based)
+	 * of the read into the gapped reference or consensus sequence.
+	 * @param direction the read direction.
+	 * @param gappedSequence the gapped sequence of this read
+	 * to align to the reference/consensus
+	 * @param readInfo the {@link ReadInfo} for this read.
+	 */
 	void aligned(String id, NucleotideSequence nucleotideSequence,
 			QualitySequence qualitySequence, PositionSequence positions,
 			URI sourceFileUri, String referenceId, long gappedStartOffset,
 			Direction direction,
 			ReferenceMappedNucleotideSequence gappedSequence,
 			ReadInfo readInfo);
-
+	/**
+	 * The command that was run to generate this assembly.
+	 * This method will only be called if the command is kown.
+	 * @param name
+	 * @param version
+	 * @param parameters
+	 */
 	void assemblyCommand(String name, String version, String parameters);
-
+	/**
+	 * The {@link URI} of the reference file
+	 * used in this assembly.
+	 * @param uri
+	 */
 	void referenceFile(URI uri);
-
+	/**
+	 * The {@link URI} of a read file
+	 * used in this assembly.  If more than one
+	 * read file was used, then this method
+	 * may be called more than once.
+	 * @param uri
+	 */
 	void readFile(URI uri);
 
 	
