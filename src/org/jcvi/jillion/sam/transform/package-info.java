3b9ce0f409571360987fb3a4c5877d56a34634f3
Dan Katzel <dkatzel@users.sourceforge.net>
2014-02-04 19:06:45 +0000

    added package info
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@2176 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

---
+++ src/org/jcvi/jillion/sam/transform/package-info.java
@@ -0,0 +1,7 @@
+/**
+ * This package transforms SAM encoded
+ * files into
+ * other assembly "contexts"
+ * using {@link org.jcvi.jillion.assembly.AssemblyTransformer}.
+ */
+package org.jcvi.jillion.sam.transform;
