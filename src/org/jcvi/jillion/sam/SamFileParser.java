cb7403cbd9de84f7e774aaf5d0c5ca53124ed859
Dan Katzel <dkatzel@gmail.com>
2014-02-11 01:26:50 +0000

    began work on BAM parser.  still need to finish the optional attributes and work out the multi-part gzip stuff
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@2180 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src/org/jcvi/jillion/sam/SamFileParser.java
+++ src/org/jcvi/jillion/sam/SamFileParser.java
@@ -3,10 +3,6 @@ package org.jcvi.jillion.sam;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.text.ParseException;
-import java.util.Date;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -25,33 +21,10 @@ import org.jcvi.jillion.sam.attribute.SamAttributeKeyFactory;
 import org.jcvi.jillion.sam.attribute.SamAttributeType;
 import org.jcvi.jillion.sam.attribute.SamAttributeValidator;
 import org.jcvi.jillion.sam.cigar.Cigar;
-import org.jcvi.jillion.sam.header.ReadGroup;
-import org.jcvi.jillion.sam.header.ReadGroup.PlatformTechnology;
-import org.jcvi.jillion.sam.header.ReferenceSequence;
 import org.jcvi.jillion.sam.header.SamHeader;
-import org.jcvi.jillion.sam.header.SamHeader.Builder;
-import org.jcvi.jillion.sam.header.SamProgram;
-import org.jcvi.jillion.sam.header.SamVersion;
-import org.jcvi.jillion.sam.header.SortOrder;
 import org.jcvi.jillion.trace.fastq.FastqQualityCodec;
 
-public class SamFileParser implements SamParser{
-
-	private static final String COMMENT_KEY = "@CO";
-
-	private static final String PROGRAM_KEY = "@PG";
-
-	private static final String READ_GROUP_KEY = "@RG";
-
-	private static final String SEQUENCE_DICTIONARY_KEY = "@SQ";
-
-
-
-	private static final String HEADER_KEY = "@HD";
-	private static final String HEADER_VERSION_TAG = "VN";
-	private static final String HEADER_SORT_TAG = "SO";
-	
-	private static final Pattern HEADER_TAG_VALUE_PATTERN = Pattern.compile("([A-Za-z][A-Za-z0-9]):([ -~]+)");
+public class SamFileParser extends AbstractSamFileParser{
 	
 	private static final Pattern SPLIT_LINE_PATTERN = Pattern.compile("\t");
 	
@@ -83,6 +56,7 @@ public class SamFileParser implements SamParser{
 		return true;
 	}
 
+	
 	@Override
 	public void accept(SamVisitor visitor) throws IOException {
 		if(visitor ==null){
@@ -179,168 +153,10 @@ public class SamFileParser implements SamParser{
 
 	}
 
-	private SamHeader parseHeader(TextLineParser parser) throws IOException {
-		
-		SamHeader.Builder headerBuilder = new SamHeader.Builder();
-		String currentLine = parser.nextLine();
-		if(currentLine.startsWith(HEADER_KEY)){
-			handleHeaderLine(currentLine, headerBuilder);
-			//next line
-			currentLine = parser.nextLine();
-		}
-		boolean inHeader = true;
-		while(currentLine !=null && inHeader){
-			String trimmedLine = currentLine.trim();
-			//not sure if blank lines allowed?
-			if(!trimmedLine.isEmpty()){
-					
-				if(currentLine.startsWith(SEQUENCE_DICTIONARY_KEY)){
-					handleSequenceDictionary(currentLine, headerBuilder);
-				}else if(currentLine.startsWith(READ_GROUP_KEY)){
-					handleReadGroup(currentLine, headerBuilder);
-				}else if(currentLine.startsWith(PROGRAM_KEY)){
-					handleProgram(currentLine, headerBuilder);
-				}else if(currentLine.startsWith(COMMENT_KEY)){
-					handleComment(currentLine, headerBuilder);
-				}else{
-					//not in header?
-					inHeader = false;
-				}
-			}
-			if(inHeader){
-				currentLine = parser.nextLine();
-			}
-		}
-		return headerBuilder.build();
-	}
-
-	private void handleComment(String line, Builder headerBuilder) {
-		//trim off first 3 characters to get rid of @CO
-		headerBuilder.addComment(line.substring(3).trim());
-		
-	}
-
-	private void handleProgram(String line, Builder headerBuilder) {
-		Map<String,String> tags = parseTags(line);
-		String id = tags.get("ID");
-		SamProgram.Builder builder = new SamProgram.Builder(id);
-		if(tags.containsKey("PN")){
-			builder.setName(tags.get("PN"));
-		}
-		if(tags.containsKey("CL")){
-			builder.setCommandLine(tags.get("CL"));
-		}
-		if(tags.containsKey("PP")){
-			builder.setPrevousProgramId(tags.get("PP"));
-		}
-		if(tags.containsKey("DS")){
-			builder.setDescription(tags.get("DS"));
-		}
-		if(tags.containsKey("VN")){
-			builder.setVersion(tags.get("VN"));
-		}
-		headerBuilder.addProgram(builder.build());
-		
-	}
-
-	private void handleReadGroup(String line, Builder headerBuilder) {
-		Map<String,String> tags = parseTags(line);
-		String id = tags.get("ID");
-		ReadGroup.Builder builder = new ReadGroup.Builder(id);
-		
-		if(tags.containsKey("CN")){
-			builder.setSequencingCenter(tags.get("CN"));
-		}
-		if(tags.containsKey("DS")){
-			builder.setDescription(tags.get("DS"));
-		}
-		if(tags.containsKey("DT")){
-			try {
-				Date date = SamUtil.toDate(tags.get("DT"));
-				builder.setRunDate(date);
-			} catch (ParseException e) {
-				throw new IllegalStateException("invalid date format" + tags.get("DT"));
-			}
-		}
-		if(tags.containsKey("FO")){
-			NucleotideSequence flowOrder = new NucleotideSequenceBuilder(tags.get("FO")).build();
-			builder.setFlowOrder(flowOrder);
-		}
-		if(tags.containsKey("KS")){
-			NucleotideSequence keySequence = new NucleotideSequenceBuilder(tags.get("KS")).build();
-			builder.setKeySequence(keySequence);
-		}
-		if(tags.containsKey("LB")){
-			builder.setLibrary(tags.get("LB"));
-		}
-		if(tags.containsKey("PG")){
-			builder.setPrograms(tags.get("PG"));
-		}
-		if(tags.containsKey("PI")){
-			int insertSize = Integer.parseInt(tags.get("PI"));
-			builder.setPredictedInsertSize(insertSize);
-		}
-		if(tags.containsKey("PL")){
-			String value = tags.get("PL");
-			PlatformTechnology platform = PlatformTechnology.valueOf(value);
-			builder.setPlatform(platform);
-		}
-		if(tags.containsKey("PU")){
-			builder.setPlatformUnit(tags.get("PU"));
-		}
-		if(tags.containsKey("SM")){
-			builder.setSampleOrPoolName(tags.get("SM"));
-		}
-		
-		headerBuilder.addReadGroup(builder.build());
-		
-	}
-
-	private void handleSequenceDictionary(String line,
-			Builder headerBuilder) {
-		Map<String,String> tags = parseTags(line);
-		
-		String name = tags.get("SN");
-		int length = Integer.parseInt(tags.get("LN"));
-		ReferenceSequence.Builder builder = new ReferenceSequence.Builder(name, length);
-		if(tags.containsKey("AS")){
-			builder.setGenomeAssemblyId(tags.get("AS"));
-		}
-		if(tags.containsKey("M5")){
-			builder.setMd5("M5");
-		}
-		if(tags.containsKey("SP")){
-			builder.setSpecies(tags.get("SP"));
-		}
-		if(tags.containsKey("UR")){
-			builder.setUri(tags.get("UR"));
-		}
-		
-		headerBuilder.addReferenceSequence(builder.build());
-		
-	}
 	
-	private Map<String, String> parseTags(String line){
-		Matcher matcher = HEADER_TAG_VALUE_PATTERN.matcher(line);
-		Map<String, String> map = new HashMap<String, String>();
-		while(matcher.find()){
-			String tag = matcher.group(1);
-			String value = matcher.group(2);
-			map.put(tag, value);
-		}
-		return map;
-	}
+	
 
-	private void handleHeaderLine(String firstLine,
-			SamHeader.Builder headerBuilder) {
-		Map<String,String> tags = parseTags(firstLine);
-		//version required
-		SamVersion version = SamVersion.parseVersion(tags.get(HEADER_VERSION_TAG));
-		headerBuilder.setVersion(version);
-		
-		headerBuilder.setSortOrder(SortOrder.parseSortOrder(tags.get(HEADER_SORT_TAG)));
-		
-	}
+	
 	
 	
 	
