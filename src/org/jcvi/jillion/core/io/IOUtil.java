877ec22695391c31dcc9cf9af2586efb34e3b4b4
Dan Katzel <dkatzel@gmail.com>
2014-02-21 19:08:24 +0000

    fixed some checkstyle warnings
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@2197 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src/org/jcvi/jillion/core/io/IOUtil.java
+++ src/org/jcvi/jillion/core/io/IOUtil.java
@@ -594,20 +594,24 @@ public final class IOUtil {
     
     
     public static short readSignedShort(InputStream in ) throws IOException {
+    	//taken from DataInputStream
         int ch1 = in.read();
         int ch2 = in.read();
-        if ((ch1 | ch2) < 0)
+        if ((ch1 | ch2) < 0){
             throw new EOFException();
+        }
         return (short)((ch1 << 8) + (ch2 << 0));
     }
     
     public static int readSignedInt(InputStream in) throws IOException {
+    	//taken from DataInputStream
         int ch1 = in.read();
         int ch2 = in.read();
         int ch3 = in.read();
         int ch4 = in.read();
-        if ((ch1 | ch2 | ch3 | ch4) < 0)
+        if ((ch1 | ch2 | ch3 | ch4) < 0){
             throw new EOFException();
+        }
         return ((ch1 << 24) + (ch2 << 16) + (ch3 << 8) + (ch4 << 0));
     }
     
