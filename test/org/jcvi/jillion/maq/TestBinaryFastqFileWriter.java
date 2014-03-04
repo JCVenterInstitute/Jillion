a3c81fcca9266ac599ba1d01e9812d12dbfede49
Dan Katzel <dkatzel@users.sourceforge.net>
2014-03-04 17:07:56 +0000

    added tests for Maq BinaryFastqFileWriter fixed bug in capping quality values.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@2230 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

---
+++ test/org/jcvi/jillion/maq/TestBinaryFastqFileWriter.java
@@ -0,0 +1,141 @@
+package org.jcvi.jillion.maq;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.Collections;
+
+import org.jcvi.jillion.core.datastore.DataStoreException;
+import org.jcvi.jillion.core.io.IOUtil.Endian;
+import org.jcvi.jillion.core.qual.QualitySequenceBuilder;
+import org.jcvi.jillion.core.residue.nt.NucleotideSequenceBuilder;
+import org.jcvi.jillion.trace.fastq.FastqDataStore;
+import org.jcvi.jillion.trace.fastq.FastqRecord;
+import org.jcvi.jillion.trace.fastq.FastqRecordBuilder;
+import org.jcvi.jillion.trace.fastq.FastqRecordWriter;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+
+public class TestBinaryFastqFileWriter {
+
+	private FastqRecord record1 = new FastqRecordBuilder("id",
+										new NucleotideSequenceBuilder("ACGT").build(), 
+										new QualitySequenceBuilder(new byte[]{20,30,40,45}).build())
+									.build();
+	@Rule 
+	public TemporaryFolder tmpDir = new TemporaryFolder();
+	
+	@Test
+	public void writeOneRecordNativeByteOrder() throws IOException, DataStoreException{
+		File out =tmpDir.newFile();
+		
+		FastqRecordWriter writer = new BinaryFastqFileWriterBuilder(out)
+											.build();
+		
+		
+		writer.write(record1);
+		writer.close();
+		
+		assertContains(record1, out);
+		
+	}
+	
+	@Test
+	public void capMaxQualityTo63() throws IOException, DataStoreException{
+		File out =tmpDir.newFile();
+		
+		FastqRecordWriter writer = new BinaryFastqFileWriterBuilder(out)
+											.build();
+		
+		FastqRecord input = new FastqRecordBuilder("id",
+									new NucleotideSequenceBuilder("ACGTT").build(), 
+									new QualitySequenceBuilder(new byte[]{40,50,60,70,80}).build())
+								.build();
+		
+		FastqRecord capped = new FastqRecordBuilder("id",
+				new NucleotideSequenceBuilder("ACGTT").build(), 
+				new QualitySequenceBuilder(new byte[]{40,50,60,63,63}).build())
+			.build();
+		
+		writer.write(input);
+		writer.close();
+		
+		assertContains(capped, out);
+		
+	}
+	
+	@Test
+	public void writeMultipleRecords() throws IOException, DataStoreException{
+		File out =tmpDir.newFile();
+		
+		FastqRecordWriter writer = new BinaryFastqFileWriterBuilder(out)
+											.build();
+		
+		
+		FastqRecord capped = new FastqRecordBuilder("id2",
+									new NucleotideSequenceBuilder("ACGTT").build(), 
+									new QualitySequenceBuilder(new byte[]{40,50,60,63,63}).build())
+								.build();
+		writer.write(record1);
+		writer.write(capped);
+		writer.close();
+		
+		assertContains(Arrays.asList(record1,capped), out, Endian.getNativeEndian());
+	}
+	
+	@Test
+	public void writeOneRecordBigEndian() throws IOException, DataStoreException{
+		File out =tmpDir.newFile();
+		
+		FastqRecordWriter writer = new BinaryFastqFileWriterBuilder(out)
+											.endian(Endian.BIG)
+											.build();
+		
+		
+		writer.write(record1);
+		writer.close();
+		
+		assertContains(record1, out, Endian.BIG);
+		
+	}
+	
+	@Test
+	public void writeOneRecordLittleEndian() throws IOException, DataStoreException{
+		File out =tmpDir.newFile();
+		
+		FastqRecordWriter writer = new BinaryFastqFileWriterBuilder(out)
+											.endian(Endian.LITTLE)
+											.build();
+		
+		
+		writer.write(record1);
+		writer.close();
+		
+		assertContains(record1, out, Endian.LITTLE);
+		
+	}
+
+	private void assertContains(FastqRecord expected, File out) throws IOException, DataStoreException {
+		assertContains(Collections.singleton(expected),out, Endian.getNativeEndian());
+		
+	}
+	private void assertContains(FastqRecord expected, File out, Endian endian) throws IOException, DataStoreException {
+		assertContains(Collections.singleton(expected),out, endian);
+		
+	}
+	private void assertContains(Collection<FastqRecord> expected, File out, Endian endian) throws IOException, DataStoreException {
+		FastqDataStore datastore = new BinaryFastqFileDataStoreBuilder(out)
+										.endian(endian)
+										.build();
+		assertEquals(expected.size(), datastore.getNumberOfRecords());
+		for(FastqRecord r : expected){
+			assertEquals(r, datastore.get(r.getId()));
+		}
+		
+	}
+	
+}
