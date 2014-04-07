0355fee5d563f54d8685180caffda0307a284bf1
Dan Katzel <dkatzel@users.sourceforge.net>
2014-04-07 15:56:57 +0000

    wrote more unit tests for BamIndexFileWriterBuilder and added javadoc and new assumeSorted() flag.
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@2287 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src/org/jcvi/jillion/sam/index/BamIndexFileWriterBuilder.java
+++ src/org/jcvi/jillion/sam/index/BamIndexFileWriterBuilder.java
@@ -2,6 +2,7 @@ package org.jcvi.jillion.sam.index;
 
 import java.io.BufferedOutputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -13,43 +14,102 @@ import org.jcvi.jillion.sam.SamParser;
 import org.jcvi.jillion.sam.SamParserFactory;
 import org.jcvi.jillion.sam.SamRecord;
 import org.jcvi.jillion.sam.SamVisitor;
+import org.jcvi.jillion.sam.SortOrder;
 import org.jcvi.jillion.sam.VirtualFileOffset;
 import org.jcvi.jillion.sam.header.SamHeader;
-
+/**
+ * {@code BamIndexFileWriterBuilder}
+ * is a class that will create a BAM
+ * index file from a sorted BAM input file.
+ * @author dkatzel
+ *
+ */
 public class BamIndexFileWriterBuilder {
 
 	
 	private boolean includeMetaData=false;
+	private boolean assumeSorted=false;
+	
 	private File outputBaiFile, inputBamFile;
 	
-	
+	/**
+	 * Create a new Builder instance with the given input BAM and 
+	 * output index files.
+	 * @param inputBamFile the input BAM file to parse and create
+	 * 			an index from; can not be null, must exist
+	 * 			and end with {@literal ".bam"}.
+	 * @param outputBaiFile the output BAM index file to write; can not be null
+	 * 			and end with {@literal ".bai"}. If the output path does not exist,
+	 * 			then the file and any non-existent parent directories will be created.
+	 * @throws IOException if there are any problems creating any missing output files
+	 * 			or directories.
+	 * @throws NullPointerException if any parameter is null.
+	 * @throws IllegalArgumentException if the file extensions aren't correct.
+	 */
 	public BamIndexFileWriterBuilder(File inputBamFile, File outputBaiFile) throws IOException{
 		if(inputBamFile == null){
-			throw new NullPointerException("output bai file can not be null");
+			throw new NullPointerException("input bam file can not be null");
 		}
 		if(outputBaiFile == null){
 			throw new NullPointerException("output bai file can not be null");
 		}
+		
 		if(!FileUtil.getExtension(inputBamFile).equalsIgnoreCase("bam")){
 			throw new IllegalArgumentException("input file is not a bam file " + inputBamFile.getAbsolutePath());
 		}
 		if(!FileUtil.getExtension(outputBaiFile).equalsIgnoreCase("bai")){
 			throw new IllegalArgumentException("input file is not a bai file " + outputBaiFile.getAbsolutePath());
 		}
+		if(!inputBamFile.exists()){
+			throw new FileNotFoundException("input bam file must exist " + inputBamFile.getAbsolutePath());
+		}
 		IOUtil.mkdirs(outputBaiFile.getParentFile());
 		this.inputBamFile = inputBamFile;
 		this.outputBaiFile = outputBaiFile;
 		
 	}
-	
+	/**
+	 * Assume that the given input BAM file contains {@link SortOrder#COORDINATE} 
+	 * sorted data even if the header says otherwise.  If not called,
+	 * by default, this value is set to {@code false}.
+	 * If this value is set to {@code false}, then during the {@link #build()}, the header
+	 * of the input BAM is checked to make sure the sort order is set to 
+	 * {@link SortOrder#COORDINATE} and will throw an {@link IllegalStateException}
+	 * if it's not.
+	 * @param assumeSorted {@code true} if the header should not be checked
+	 * and assumed to be {@link SortOrder#COORDINATE}; {@code false} if the header
+	 * should be checked and fail if not (the default).
+	 * @return this.
+	 */
+	public BamIndexFileWriterBuilder assumeSorted(boolean assumeSorted){
+		this.assumeSorted = assumeSorted;
+		return this;
+	}
+	/**
+	 * BAM Index files created by some other SAM libraries add additional
+	 * metadata to the index such as the number of unaligned reads. This metadata
+	 * is not specified in the BAM Index specification so some SAM parsers might not be able to correctly
+	 * handle them.  If this method is not called, then by default, the metadata is not included.
+	 *
+	 * @param includeMetaData {@code true} if metadata should be included;
+	 * {@code false} otherwise (the default).
+	 * @return this.
+	 */
 	public BamIndexFileWriterBuilder includeMetaData(boolean includeMetaData){
 		this.includeMetaData = includeMetaData;
 		return this;
 	}
-	
+	/**
+	 * Actually parse the input BAM file and write out the corresponding
+	 * BAM index file to the given output file.
+	 * @throws IOException if there are any problems parsing the 
+	 * BAM file or writing out the index file.
+	 * @throws IllegalStateException if the BAM file is not in {@link SortOrder#COORDINATE}
+	 * order and {@link #assumeSorted(boolean)} is set to {@code false}.
+	 */
 	public void build() throws IOException{
 		SamParser parser = SamParserFactory.create(inputBamFile);
-		BamIndexSamVisitor visitor = new BamIndexSamVisitor();
+		BamIndexSamVisitor visitor = new BamIndexSamVisitor(assumeSorted);
 		parser.accept(visitor);
 		
 		OutputStream out =null;
@@ -64,7 +124,13 @@ public class BamIndexFileWriterBuilder {
 	
 	private static class BamIndexSamVisitor implements SamVisitor {
 		private BamIndexer indexer;
+		private final boolean assumeSorted;
 		
+		
+
+		public BamIndexSamVisitor(boolean assumeSorted) {
+			this.assumeSorted = assumeSorted;
+		}
 
 		@Override
 		public void visitRecord(SamVisitorCallback callback, SamRecord record,
@@ -81,6 +147,9 @@ public class BamIndexFileWriterBuilder {
 		
 		@Override
 		public void visitHeader(SamVisitorCallback callback, SamHeader header) {
+			if(!assumeSorted && !header.getSortOrder().equals(SortOrder.COORDINATE)){
+				throw new IllegalStateException("bam file not in coordinate sort order : " + header.getSortOrder());
+			}
 			indexer = new BamIndexer(header);
 		}
 		
--- test/org/jcvi/jillion/sam/index/TestBamIndexWriter.java
+++ test/org/jcvi/jillion/sam/index/TestBamIndexWriter.java
@@ -1,10 +1,25 @@
 package org.jcvi.jillion.sam.index;
 
-import java.io.File;
-import java.io.IOException;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Arrays;
+
+import org.jcvi.jillion.core.io.IOUtil;
 import org.jcvi.jillion.core.testUtil.TestUtil;
 import org.jcvi.jillion.internal.ResourceHelper;
+import org.jcvi.jillion.sam.SamParserFactory;
+import org.jcvi.jillion.sam.SamRecord;
+import org.jcvi.jillion.sam.SamVisitor;
+import org.jcvi.jillion.sam.SamWriter;
+import org.jcvi.jillion.sam.SamWriterBuilder;
+import org.jcvi.jillion.sam.SortOrder;
+import org.jcvi.jillion.sam.VirtualFileOffset;
+import org.jcvi.jillion.sam.header.SamHeader;
+import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
@@ -15,17 +30,246 @@ public class TestBamIndexWriter {
 	@Rule
 	public TemporaryFolder tmpDir = new TemporaryFolder();
 	
+	private File bamFile, expectedBaiFile;
+	
+	@Before
+	public void setup() throws IOException{
+		bamFile = resources.getFile("index_test.bam");
+		expectedBaiFile = resources.getFile("index_test.bam.bai");
+	}
+	
+	@Test(expected = NullPointerException.class)
+	public void nullInputFileShouldThrowNPE() throws IOException{
+		
+		new BamIndexFileWriterBuilder(null, new File("a.bai"));
+	}
+	@Test(expected = NullPointerException.class)
+	public void nullOutputFileShouldThrowNPE() throws IOException{
+		
+		new BamIndexFileWriterBuilder(new File("a.bam"), null);
+	}
+	
+	@Test(expected = IllegalArgumentException.class)
+	public void wrongExtensionForOutputFileShouldThrowException() throws IOException{
+		
+		new BamIndexFileWriterBuilder(new File("a.bam"), new File("notABai"));
+	}
+	@Test(expected = IllegalArgumentException.class)
+	public void wrongExtensionForInputFileShouldThrowException() throws IOException{
+		
+		new BamIndexFileWriterBuilder(new File("notABam"), new File("a.bai"));
+	}
+	
+	@Test(expected = FileNotFoundException.class)
+	public void inputFileDoesNotExistShouldThrowFileNotFoundException() throws IOException{
+		File nonExistentFile = new File(tmpDir.getRoot(), "dne.bam");
+		new BamIndexFileWriterBuilder(nonExistentFile, new File("a.bai"));
+	}
+	
 	@Test
 	public void writterWithMetaDataMatchesByteForByteToPicard() throws IOException{
-		File bamfile = resources.getFile("index_test.bam");
-		File expectedBaiFile = resources.getFile("index_test.bam.bai");
+		
 		
 		File actualBaiFile = tmpDir.newFile("actual.bai");
 		
-		new BamIndexFileWriterBuilder(bamfile, actualBaiFile)
+		new BamIndexFileWriterBuilder(bamFile, actualBaiFile)
 					.includeMetaData(true)
 					.build();
 		
-		TestUtil.contentsAreEqual(expectedBaiFile, actualBaiFile);
+		assertTrue(TestUtil.contentsAreEqual(expectedBaiFile, actualBaiFile));
+	}
+	
+	@Test
+	public void writingNonCoordinateSortedBamShouldThrowException() throws IOException{
+
+		SamHeader originalHeader = parseSamHeaderFrom(bamFile);
+		
+		File incorrectlySortedFile = tmpDir.newFile("wrongSort.bam");
+		File actualBaiFile = tmpDir.newFile("actual.bai");
+		for(SortOrder incorrectSortOrder : Arrays.asList(SortOrder.QUERY_NAME, SortOrder.UNKNOWN, SortOrder.UNSORTED)){
+			SamWriter writer = new SamWriterBuilder(incorrectlySortedFile, originalHeader)
+														.forceHeaderSortOrder(incorrectSortOrder)
+														.build();
+			
+			writeAllRecords(bamFile, writer);
+			verifyIndexWriterThrowsException(incorrectlySortedFile,	actualBaiFile, incorrectSortOrder);
+		}
+	}
+	@Test
+	public void assumeSortedFlagSetWillWriteIndexEvenIfHeaderSaysOtherwise() throws IOException{
+
+		SamHeader originalHeader = parseSamHeaderFrom(bamFile);
+		
+		File incorrectlySortedFile = tmpDir.newFile("wrongSort.bam");
+
+		//the input bam file is from an old version of picard
+		//which didn't compress as well (at all?)
+		//so since we are writing out bams using compression,
+		//also the different header sort order cause the header
+		//length to be different so we have to compensate for that as well.
+		//(see createHeader() method for more details)
+		//Therefore, we need to write out a new file so that our
+		//indexes will match.		
+		File newBam = reWriteBam(bamFile, createHeader(originalHeader, SortOrder.COORDINATE));
+		File expectedBai = createIndex(newBam);
+		for(SortOrder incorrectSortOrder : Arrays.asList(SortOrder.QUERY_NAME, SortOrder.UNKNOWN, SortOrder.UNSORTED)){
+			SamWriter writer = new SamWriterBuilder(incorrectlySortedFile, createHeader(originalHeader, incorrectSortOrder))
+														.forceHeaderSortOrder(incorrectSortOrder)
+														.build();
+			
+			writeAllRecords(newBam, writer);
+			File actualBai = createIndex(incorrectlySortedFile, incorrectSortOrder);
+			assertTrue(TestUtil.contentsAreEqual(expectedBai, actualBai));
+		}
+	}
+
+	private SamHeader createHeader(SamHeader originalHeader, SortOrder order){
+		SamHeader.Builder builder = new SamHeader.Builder(originalHeader);
+		//because the index uses byte offsets,
+		//the different sort orders will cause the header to be different
+		//byte lengths which will throw off the index offsets by a few bytes.
+		//To get around this, we will add a comment String to the header
+		//that is a variable amount depending on the sort order so that
+		//all the records start at the same byte offset.
+		//since coordinate sort is the largest word, we will padd compared to that.
+		if(order ==SortOrder.COORDINATE){
+			builder.addComment("");
+		}else{
+			int padding = SortOrder.COORDINATE.getEncodedName().length() -order.getEncodedName().length();
+			StringBuilder paddedString = new StringBuilder(padding);
+			for(int i=0; i<padding; i++){
+				paddedString.append("*");
+			}
+			builder.addComment(paddedString.toString());
+		}
+		return builder.build();
+		
+	}
+	
+	
+	private File reWriteBam(File bamFile, SamHeader header) throws IOException{
+		File newBam = tmpDir.newFile(bamFile.getName());
+		
+		SamWriter writer = new SamWriterBuilder(newBam, header)
+								.build();
+
+		writeAllRecords(bamFile, writer);
+		return newBam;
+		
+	}
+	
+	
+	private File createIndex(File inputBam) throws IOException{
+		return createIndex(inputBam, null);
+	}
+	private File createIndex(File inputBam, SortOrder order) throws IOException{
+		StringBuilder baiName = new StringBuilder(inputBam.getName());
+		if(order!=null){
+			baiName.append(".").append(order);
+		}
+		baiName.append(".bai");
+		
+		File outputBai = tmpDir.newFile(baiName.toString());
+		new BamIndexFileWriterBuilder(inputBam, outputBai)
+						.assumeSorted(true)
+						.includeMetaData(true)
+						.build();
+		
+		return outputBai;
+	}
+
+	private void verifyIndexWriterThrowsException(File incorrectlySortedFile,
+			File actualBaiFile, SortOrder sortOrder) throws IOException {
+		try{
+			new BamIndexFileWriterBuilder(incorrectlySortedFile, actualBaiFile)
+					.build();
+			fail("should throw IllegalStateException when " + sortOrder);
+		}catch(IllegalStateException expected){
+			//expected
+		}
+	}
+	
+	private void writeAllRecords(File bamFile, final SamWriter writer) throws IOException {
+		try{
+			SamParserFactory.create(bamFile)
+			.accept(new SamVisitor() {
+				
+				@Override
+				public void visitRecord(SamVisitorCallback callback, SamRecord record,
+						VirtualFileOffset start, VirtualFileOffset end) {
+					try {
+						writer.writeRecord(record);
+					} catch (IOException e) {
+						throw new IllegalStateException("error writing out record",e);
+					}
+					
+				}
+				
+				@Override
+				public void visitRecord(SamVisitorCallback callback, SamRecord record) {
+					try {
+						writer.writeRecord(record);
+					} catch (IOException e) {
+						throw new IllegalStateException("error writing out record",e);
+					}
+				}
+				
+				@Override
+				public void visitHeader(SamVisitorCallback callback, SamHeader header) {
+					//no-op
+				}
+				
+				@Override
+				public void visitEnd() {
+					//no-op
+				}
+				
+				@Override
+				public void halted() {
+					//no-op
+				}
+			});
+		}finally{
+			IOUtil.closeAndIgnoreErrors(writer);
+		}
+		
+	}
+
+	private SamHeader parseSamHeaderFrom(File samOrBam) throws IOException{
+		//create final array so we can reference it in our 
+		//anonymous class
+		final SamHeader[] singleHeaderBuilder = new SamHeader[1];
+		SamParserFactory.create(samOrBam)
+						.accept(new SamVisitor() {
+							
+							@Override
+							public void visitRecord(SamVisitorCallback callback, SamRecord record,
+									VirtualFileOffset start, VirtualFileOffset end) {
+								//no-op								
+							}
+							
+							@Override
+							public void visitRecord(SamVisitorCallback callback, SamRecord record) {
+								//no-op
+							}
+							
+							@Override
+							public void visitHeader(SamVisitorCallback callback, SamHeader header) {
+								singleHeaderBuilder[0] = header;
+								callback.haltParsing();
+							}
+							
+							@Override
+							public void visitEnd() {
+								//no-op
+							}
+							
+							@Override
+							public void halted() {
+								//no-op
+							}
+						});
+		
+		return singleHeaderBuilder[0];
 	}
 }
