0ea812d47babe6f3e0905e0da319d46030d6dd5f
Dan Katzel <dkatzel@gmail.com>
2014-06-12 18:23:25 +0000

    added tests for blast xml without any hits but had iterations
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@2320 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- experimental-test/org/jcvi/jillion_experimental/align/blast/AllBlastUnitTests.java
+++ experimental-test/org/jcvi/jillion_experimental/align/blast/AllBlastUnitTests.java
@@ -30,7 +30,9 @@ import org.junit.runners.Suite.SuiteClasses;
         TestBlastHit.class,
         TestTabularBlastParser.class,
         TestTabularRotaBlastResults.class,
-        TestXmlRotaBlastResults.class
+        TestXmlRotaBlastResults.class,
+        
+        TestBlastXmlWithNoHits.class
     }
     )
 public class AllBlastUnitTests {
