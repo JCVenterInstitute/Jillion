bde00699f4a7b397377bb215560332d4056abb95
Dan Katzel <dkatzel@users.sourceforge.net>
2014-08-21 18:22:54 +0000

    added serial version id
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@2346 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src/org/jcvi/jillion/internal/core/util/ValidationException.java
+++ src/org/jcvi/jillion/internal/core/util/ValidationException.java
@@ -2,6 +2,9 @@ package org.jcvi.jillion.internal.core.util;
 
 public class ValidationException extends IllegalStateException {
 
+
+	private static final long serialVersionUID = 1L;
+
 	public ValidationException(String message, Throwable cause) {
 		super(message, cause);
 	}
