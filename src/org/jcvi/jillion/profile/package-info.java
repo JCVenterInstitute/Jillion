2cd9692c287ede6dcf11d4a04675e45cb84316de
Dan Katzel <dkatzel@gmail.com>
2014-09-09 16:17:56 +0000

    replaced accented character which wasn't UTF-8
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@2362 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src/org/jcvi/jillion/profile/package-info.java
+++ src/org/jcvi/jillion/profile/package-info.java
@@ -5,6 +5,6 @@
  *
  *@see <a href="http://andestools.sourceforge.net/">ANDES sourceforge page</a>
  *@see <a href="http://www.biomedcentral.com/1756-0500/3/199">
- *Kelvin Li, Eli Venter, Shibu Yooseph, Timothy B Stockwell, Lance D Eckerle, Mark R Denison, David J Spiro and Barbara A Methé, "ANDES: Statistical tools for the ANalyses of DEep Sequencing", BMC Research Notes 2010, 3:199, doi:10.1186/1756-0500-3-199</a>
+ *Kelvin Li, Eli Venter, Shibu Yooseph, Timothy B Stockwell, Lance D Eckerle, Mark R Denison, David J Spiro and Barbara A Methe, "ANDES: Statistical tools for the ANalyses of DEep Sequencing", BMC Research Notes 2010, 3:199, doi:10.1186/1756-0500-3-199</a>
  */
 package org.jcvi.jillion.profile;
