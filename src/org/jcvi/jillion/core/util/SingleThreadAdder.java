904aaff3d4250f4f11c6fc90fa1a38bd8b31ff74
Dan Katzel <dkatzel@gmail.com>
2014-10-02 19:41:08 +0000

    Added singleThreadAdder utility class
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@2367 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

---
+++ src/org/jcvi/jillion/core/util/SingleThreadAdder.java
@@ -0,0 +1,51 @@
+package org.jcvi.jillion.core.util;
+/**
+ * {@code SingleThreadAdder}
+ * is an equivalent class to {@link java.util.concurrent.atomic.LongAdder}
+ * but is not threadsafe.  This should be more performant than  
+ * using LongAdder but should only be used 
+ * by a single thread.
+ * @author dkatzel
+ *
+ */
+public class SingleThreadAdder {
+
+	private long value;
+	
+	public SingleThreadAdder(){
+		value = 0;
+	}
+	
+	public SingleThreadAdder(long initialValue){
+		value = initialValue;
+	}
+	
+	/**
+     * Adds the given value.
+     *
+     * @param x the value to add
+     */
+    public void add(long x) {
+    	value +=x;
+    }
+    /**
+     * Equivalent to {@code add(1)}.
+     */
+	public void increment(){
+		value++;
+	}
+	/**
+     * Equivalent to {@code add(-1)}.
+     */
+	public void decrement(){
+		value--;
+	}
+	
+	public long longValue(){
+		return value;
+	}
+	
+	public int intValue(){		
+		return (int)value;
+	}
+}
