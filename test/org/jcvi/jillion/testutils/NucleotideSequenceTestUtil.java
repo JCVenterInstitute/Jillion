19c7d794e96ca3c3b1a5a9648a955464a7ca0734
Dan Katzel <dkatzel@gmail.com>
2014-12-30 19:26:29 +0000

    added emptySequence method
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@2405 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- test/org/jcvi/jillion/testutils/NucleotideSequenceTestUtil.java
+++ test/org/jcvi/jillion/testutils/NucleotideSequenceTestUtil.java
@@ -5,10 +5,13 @@ import org.jcvi.jillion.core.residue.nt.NucleotideSequenceBuilder;
 
 public final class NucleotideSequenceTestUtil {
 
+	private static final NucleotideSequence EMPTY = new NucleotideSequenceBuilder("").build();
 	private NucleotideSequenceTestUtil(){
 		//can not instantiate
 	}
-	
+	public static NucleotideSequence emptySeq(){
+		return EMPTY;
+	}
 	public static NucleotideSequence create(String seq){
 		return new NucleotideSequenceBuilder(seq).build();
 	}
