3637c965740d32364be7e9b4f0e95b2cc5cd4f15
Dan Katzel <dkatzel@users.sourceforge.net>
2015-01-07 19:56:34 +0000

    added javadoc
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@2412 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src/org/jcvi/jillion/testutils/NucleotideSequenceTestUtil.java
+++ src/org/jcvi/jillion/testutils/NucleotideSequenceTestUtil.java
@@ -2,16 +2,39 @@ package org.jcvi.jillion.testutils;
 
 import org.jcvi.jillion.core.residue.nt.NucleotideSequence;
 import org.jcvi.jillion.core.residue.nt.NucleotideSequenceBuilder;
-
+/**
+ * Utility class for making tests with
+ * {@link NucleotideSequence}s more readable.
+ * @author dkatzel
+ *
+ */
 public final class NucleotideSequenceTestUtil {
 
 	private static final NucleotideSequence EMPTY = new NucleotideSequenceBuilder("").build();
 	private NucleotideSequenceTestUtil(){
 		//can not instantiate
 	}
+	/**
+	 * Get a singleton empty {@link NucleotideSequence} instance
+	 * (whose length is 0 ).
+	 * @return a {@link NucleotideSequence}; will never be null.
+	 */
 	public static NucleotideSequence emptySeq(){
 		return EMPTY;
 	}
+	/**
+	 * Create a new {@link NucleotideSequence}
+	 * using the given String.
+	 * 
+	 * @apiNote This is identical to
+	 * <code>new NucleotideSequenceBuilder(seq).build();</code>
+	 * 
+	 * @param seq The sequence as a String; can not be null.
+	 * @return a new NucleotideSequence will never be null.
+	 * 
+	 * @throws NullPointerException if seq is null.
+	 * @throws IllegalArgumentException if seq has any invalid characters.
+	 */
 	public static NucleotideSequence create(String seq){
 		return new NucleotideSequenceBuilder(seq).build();
 	}
--- src/org/jcvi/jillion/testutils/assembly/cas/package-info.java
+++ src/org/jcvi/jillion/testutils/assembly/cas/package-info.java
@@ -1,8 +1,8 @@
 /**
+ * Utility classes for testing 
+ * CLC Reference Assembled data CAS formatted data.
  * 
- */
-/**
- * @author danny
+ * @author dkatzel
  *
  */
 package org.jcvi.jillion.testutils.assembly.cas;
