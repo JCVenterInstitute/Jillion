bf1480153dfce717aefe4470df57f34c090edde9
Dan Katzel <dkatzel@gmail.com>
2015-01-14 16:44:55 +0000

    added more helper methods
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@2413 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src/org/jcvi/jillion/testutils/NucleotideSequenceTestUtil.java
+++ src/org/jcvi/jillion/testutils/NucleotideSequenceTestUtil.java
@@ -1,5 +1,8 @@
 package org.jcvi.jillion.testutils;
 
+import java.util.Random;
+
+import org.jcvi.jillion.core.residue.nt.Nucleotide;
 import org.jcvi.jillion.core.residue.nt.NucleotideSequence;
 import org.jcvi.jillion.core.residue.nt.NucleotideSequenceBuilder;
 /**
@@ -11,6 +14,9 @@ import org.jcvi.jillion.core.residue.nt.NucleotideSequenceBuilder;
 public final class NucleotideSequenceTestUtil {
 
 	private static final NucleotideSequence EMPTY = new NucleotideSequenceBuilder("").build();
+	
+	private static final Random random = new Random();
+	
 	private NucleotideSequenceTestUtil(){
 		//can not instantiate
 	}
@@ -36,6 +42,44 @@ public final class NucleotideSequenceTestUtil {
 	 * @throws IllegalArgumentException if seq has any invalid characters.
 	 */
 	public static NucleotideSequence create(String seq){
-		return new NucleotideSequenceBuilder(seq).build();
+		return new NucleotideSequenceBuilder(seq)
+							.turnOffDataCompression(true)
+							.build();
+	}
+	
+	public static NucleotideSequence create(String s, int times){
+		int motifLength = s.length();
+		NucleotideSequenceBuilder builder= new NucleotideSequenceBuilder(motifLength *times)
+													.turnOffDataCompression(true);
+		for(int i=0; i<times; i++){
+			builder.append(s);
+		}
+		return builder.build();
+	}
+	/**
+	 * Create a new {@link NucleotideSequence} of the given
+	 * length that is randomly populated with A, C, G and Ts.
+	 * @param length the length of the sequence to return; must be >0
+	 * @return a new {@link NucleotideSequence}; will never be null or 
+	 * and always the specified length;
+	 * @throws IllegalArgumentException if length <1
+	 */
+	public static NucleotideSequence createRandom(int length){
+		NucleotideSequenceBuilder builder = new NucleotideSequenceBuilder(length);
+		for(int i=0; i<length; i++){
+			builder.append(nextRandom());
+		}
+		builder.turnOffDataCompression(true);
+		return builder.build();
+	}
+	
+	private static Nucleotide nextRandom(){
+		switch(random.nextInt(4)){
+			case 0: return Nucleotide.Adenine;
+			case 1: return Nucleotide.Cytosine;
+			case 2: return Nucleotide.Guanine;
+			default : return Nucleotide.Thymine;
+			
+		}
 	}
 }
