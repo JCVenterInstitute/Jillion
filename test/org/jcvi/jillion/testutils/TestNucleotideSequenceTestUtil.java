4be0ee50bc208ad2db3fc48d96451d5ca48f0319
Dan Katzel <dkatzel@gmail.com>
2015-01-14 16:55:14 +0000

    added more tests for new methods
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@2414 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- test/org/jcvi/jillion/testutils/TestNucleotideSequenceTestUtil.java
+++ test/org/jcvi/jillion/testutils/TestNucleotideSequenceTestUtil.java
@@ -1,9 +1,15 @@
 package org.jcvi.jillion.testutils;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.jcvi.jillion.core.residue.nt.Nucleotide;
+import org.jcvi.jillion.core.residue.nt.NucleotideSequence;
 import org.jcvi.jillion.core.residue.nt.NucleotideSequenceBuilder;
 import org.junit.Test;
-
-import static org.junit.Assert.*;
 public class TestNucleotideSequenceTestUtil {
 
 	@Test
@@ -13,7 +19,40 @@ public class TestNucleotideSequenceTestUtil {
 	}
 	
 	@Test
+	public void createWithTimes(){		
+		assertEquals("ACGTACGTACGTACGTACGT", NucleotideSequenceTestUtil.create("ACGT", 5).toString());
+	}
+	
+	@Test
 	public void empty(){
 		assertTrue(NucleotideSequenceTestUtil.emptySeq().getLength() ==0);
+	}
+	
+	@Test
+	public void randomSeq(){
+		int length = 30;
+		Set<NucleotideSequence> seqs = new HashSet<>();
+		for(int i=0; i<10; i++){
+			NucleotideSequence seq = NucleotideSequenceTestUtil.createRandom(length);
+			
+			assertEquals(length, seq.getLength());
+			onlyContainsACGT(seq);
+			seqs.add(seq);
+		}
+		
+		assertTrue("random seqs returns are not random",seqs.size() >1);
+	}
+
+	private void onlyContainsACGT(NucleotideSequence seq) {
+		for(Nucleotide n : seq){
+			switch (n){
+			case Adenine:
+			case Guanine:
+			case Cytosine:
+			case Thymine: break;
+			default : throw new AssertionError("seq contains non ACGT : " + n);
+			}
+		}
+		
 	}
 }
