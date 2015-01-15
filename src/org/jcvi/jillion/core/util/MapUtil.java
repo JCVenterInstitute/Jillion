771468d737db804dc785486700d5f29353a544fd
Dan Katzel <dkatzel@users.sourceforge.net>
2015-01-15 16:49:21 +0000

    added method that takes load factor
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@2415 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src/org/jcvi/jillion/core/util/MapUtil.java
+++ src/org/jcvi/jillion/core/util/MapUtil.java
@@ -20,8 +20,6 @@
  ******************************************************************************/
 package org.jcvi.jillion.core.util;
 
-import java.util.HashMap;
-import java.util.Map;
 /**
  * Utility class for working with {@link Map}s.
  * @author dkatzel
@@ -51,12 +49,36 @@ public final class MapUtil {
 	 * > {@link Integer#MAX_VALUE}.
 	 */
 	public static int computeMinHashMapSizeWithoutRehashing(long expectedNumberOfEntries){
+		return computeMinHashMapSizeWithoutRehashing(expectedNumberOfEntries, DEFAULT_LOAD_FACTOR);
+	}
+	
+	/**
+	 * Computes the smallest possible 
+	 * size of a {@link HashMap} using the default
+	 * load factor that will not cause the map to 
+	 * perform an expensive rehashing operation (which also
+	 * doubles the number of buckets used).
+	 * @param expectedNumberOfEntries the expected number
+	 * of entries that will be put into the map.
+	 * This type is long to help clients avoid
+	 * casting but the max value allowed is {@link Integer#MAX_VALUE}.
+	 * Passing in a value greater than {@link Integer#MAX_VALUE} will
+	 * throw an Exception.  (This is better than having
+	 * clients downcast and possibly truncate the value without any error).
+	 * @return the initial size value to pass to the HashMap constructor.
+	 * @throws IllegalArgumentException if expectedNumberOfEntries is <0 or
+	 * > {@link Integer#MAX_VALUE}.
+	 */
+	public static int computeMinHashMapSizeWithoutRehashing(long expectedNumberOfEntries, float loadFactor){
 		if(expectedNumberOfEntries <0){
 			throw new IllegalArgumentException("number of entries must be >=0");
 		}
 		if(expectedNumberOfEntries > Integer.MAX_VALUE){
 			throw new IllegalArgumentException("number of entries must be <= Integer.MAX_VALUE");
 		}
-		return (int)(expectedNumberOfEntries/DEFAULT_LOAD_FACTOR +1);
+		if(loadFactor <0 || loadFactor> 1){
+			throw new IllegalArgumentException("invalid load factor, must be between 0 and 1 : "+ loadFactor);
+		}
+		return (int)(expectedNumberOfEntries/loadFactor +1);
 	}
 }
