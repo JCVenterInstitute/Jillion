011ee867b89abf989420f3c15e9ee24efbfccf9c
Dan Katzel <dkatzel@users.sourceforge.net>
2015-06-02 19:47:12 +0000

    changed lambda expression into method reference
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@2441 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src/org/jcvi/jillion/fasta/FastaRecordDataStoreAdapter.java
+++ src/org/jcvi/jillion/fasta/FastaRecordDataStoreAdapter.java
@@ -48,18 +48,8 @@ public final class FastaRecordDataStoreAdapter{
      * @return a new {@code DataStore<T>} which wraps the given datastore. 
      */
     public static <S,T extends Sequence<S>,F extends FastaRecord<S,T>, D extends DataStore<T>> D adapt(Class<D> datastoreToMimic, DataStore<F> datastoreOfFastaRecords){
-      /* return DataStoreUtil.adapt(datastoreToMimic, datastoreOfFastaRecords, new DataStoreUtil.AdapterCallback<F, T>() {
-
-		@Override
-		public T get(F from) {
-			return from.getSequence();
-		}
-    	   
-       });
-       */
-    	
     	return DataStoreUtil.adapt(datastoreToMimic, datastoreOfFastaRecords,
-    			from-> 	from.getSequence()        	   
+    			FastaRecord::getSequence      	   
            );
     }
     
