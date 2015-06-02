d48556855811eb37fd4fe7560cb529f4e950dca2
Dan Katzel <dkatzel@gmail.com>
2015-06-02 15:38:41 +0000

    fixed some PMD warnings
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@2438 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src/org/jcvi/jillion/assembly/ReAlignReads.java
+++ src/org/jcvi/jillion/assembly/ReAlignReads.java
@@ -179,19 +179,25 @@ public class ReAlignReads {
 
 		@Override
 		public boolean equals(Object obj) {
-			if (this == obj)
+			if (this == obj){
 				return true;
-			if (obj == null)
+			}
+			if (obj == null){
 				return false;
-			if (getClass() != obj.getClass())
+			}
+			if (getClass() != obj.getClass()){
 				return false;
+			}
 			ReAlignResult other = (ReAlignResult) obj;
-			if (gappedStartOffset != other.gappedStartOffset)
+			if (gappedStartOffset != other.gappedStartOffset){
 				return false;
-			if (!gappedValidBases.equals(other.gappedValidBases))
+			}
+			if (!gappedValidBases.equals(other.gappedValidBases)){
 				return false;
-			if (!validRange.equals(other.validRange))
+			}
+			if (!validRange.equals(other.validRange)){
 				return false;
+			}
 			return true;
 		}
 		
