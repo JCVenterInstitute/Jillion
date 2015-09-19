561823eceef70294bf722110aee78d25f7a49c90
Dan Katzel <dkatzel@gmail.com>
2015-09-19 23:00:01 +0000

    updated Jillion 5 to use LGPL 2.1 instead of GPL 3
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@2552 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src/org/jcvi/jillion/core/residue/Frame.java
+++ src/org/jcvi/jillion/core/residue/Frame.java
@@ -1,3 +1,23 @@
+/*******************************************************************************
+ * Jillion development code
+ * 
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public License.  This should
+ * be distributed with the code.  If you do not have a copy,
+ *  see:
+ * 
+ *          http://www.gnu.org/copyleft/lesser.html
+ * 
+ * 
+ * Copyright for this code is held jointly by the individual authors.  These should be listed in the @author doc comments.
+ * 
+ * Information about Jillion can be found on its homepage
+ * 
+ *         http://jillion.sourceforge.net
+ * 
+ * Contributors:
+ *     Danny Katzel - initial API and implementation
+ ******************************************************************************/
 package org.jcvi.jillion.core.residue;
 
 public enum Frame{
