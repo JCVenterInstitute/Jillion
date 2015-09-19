881ec092f996e94cb38aa361089e92482497be1e
Dan Katzel <dkatzel@users.sourceforge.net>
2015-09-19 23:00:01 +0000

    updated Jillion 5 to use LGPL 2.1 instead of GPL 3
    git-svn-id: svn+ssh://svn.code.sf.net/p/jillion/code/trunk@2552 d1f56cec-0f3b-4699-95c7-aedf87c10f2a

--- src/org/jcvi/jillion/sam/attribute/SamAttributeKey.java
+++ src/org/jcvi/jillion/sam/attribute/SamAttributeKey.java
@@ -1,23 +1,22 @@
 /*******************************************************************************
- * Copyright (c) 2009 - 2015 J. Craig Venter Institute.
- * 	This file is part of Jillion
+ * Jillion development code
  * 
- * 	Jillion is free software: you can redistribute it and/or modify
- * 	it under the terms of the GNU General Public License as published by
- * 	the Free Software Foundation, either version 3 of the License, or
- * 	(at your option) any later version.
- * 	
- * 	Jillion is distributed in the hope that it will be useful,
- * 	but WITHOUT ANY WARRANTY; without even the implied warranty of
- * 	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * 	GNU General Public License for more details.
- * 	
- * 	You should have received a copy of the GNU General Public License
- * 	along with Jillion.  If not, see <http://www.gnu.org/licenses/>.
- * 	
- * 	
- * 	Contributors:
- *         Danny Katzel - initial API and implementation
+ * This code may be freely distributed and modified under the
+ * terms of the GNU Lesser General Public License.  This should
+ * be distributed with the code.  If you do not have a copy,
+ *  see:
+ * 
+ *          http://www.gnu.org/copyleft/lesser.html
+ * 
+ * 
+ * Copyright for this code is held jointly by the individual authors.  These should be listed in the @author doc comments.
+ * 
+ * Information about Jillion can be found on its homepage
+ * 
+ *         http://jillion.sourceforge.net
+ * 
+ * Contributors:
+ *     Danny Katzel - initial API and implementation
  ******************************************************************************/
 package org.jcvi.jillion.sam.attribute;
 
